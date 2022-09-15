14992e8b85f6ce16961879c7980d373c1ccd7e5d
oncsr <khj20006@naver.com>
2022-09-15 21:49:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 52 ms, Memory: 10964 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, int> edge;	// 간선
+
+int V, E, A, B, C;	// V = 정점 수, E = 간선 수
+int visit[10001] = { 0 };	// 방문 여부 확인
+vector<vector<edge> > graph(10001);	// 그래프
+priority_queue<edge, vector<edge>, greater<> > Q;
+// 탐색 우선순위 저장
+int cnt = 0;	// 탐색한 간선의 수
+ll cost = 0;	// 비용
+
+void dfs(int node) {
+	if (!visit[node]) {
+		visit[node]++;
+		while (cnt < V - 1) {
+			for (int i = 0; i < graph[node].size(); i++)
+				if (!visit[graph[node][i].second])
+					Q.push(graph[node][i]);
+			int next = Q.top().second;
+			int money = Q.top().first;
+			Q.pop();
+			while (!Q.empty() && visit[next]) {
+				next = Q.top().second;
+				money = Q.top().first;
+				Q.pop();
+			}
+			cost += money;
+			cnt++;
+			dfs(next);
+		}
+	}
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> V >> E;
+	for (int i = 0; i < E; i++) {
+		cin >> A >> B >> C;
+		graph[A].push_back({ C,B });
+		graph[B].push_back({ C,A });
+	}
+	dfs(1);
+	cout << cost << '\n';
+}
