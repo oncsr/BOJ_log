7689ce6f0248058f21707e3cfcbab4e06852d1e8
oncsr <khj20006@naver.com>
2022-09-17 19:57:53 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 244 ms, Memory: 9712 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,15 +1,22 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
+using ll = long long;
 
-int main(void)
-{
-	int n; cin >> n;
-	int arr[1000001];
-	for(int i=0; i < n;i++)
-		cin >> arr[i];
-	sort(arr, arr + n);
-	for(int i=0; i < n;i++)
-		cout << arr[i] << '\n';
-	return 0;
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, a, mn = 1000001, mx = -1000001;
+	cin >> N;
+	int arr[2000001]{};
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		mn = min(a, mn);
+		mx = max(a, mx);
+		arr[a + 1000000]++;
+	}
+	for (int i = mn + 1000000; i <= mx + 1000000; i++) {
+		while (arr[i]) {
+			cout << i - 1000000 << '\n';
+			arr[i]--;
+		}
+	}
 }
