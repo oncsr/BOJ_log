b87647bc244d33fb0b3fb080c18dcc9e35556cac
oncsr <khj20006@naver.com>
2022-09-17 19:52:41 +0900

    [Gold II] Title: 피보나치 수 3, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+using namespace std;
+typedef long long ll;
+const ll MOD = 1000000;
+
+ll fib(ll n) {
+	static map<ll, ll> M;
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else {
+		if (n % 2) {
+			if (!M[n / 2])
+				M[n / 2] = fib(n / 2);
+			if (!M[n / 2 + 1])
+				M[n / 2 + 1] = fib(n / 2 + 1);
+			return M[n / 2] % MOD * M[n / 2] % MOD + M[n / 2 + 1] % MOD * M[n / 2 + 1] % MOD;
+		}
+		else {
+			if (!M[n / 2])
+				M[n / 2] = fib(n / 2);
+			if (!M[n / 2 - 1])
+				M[n / 2 - 1] = fib(n / 2 - 1);
+			return M[n / 2] % MOD * (2 * M[n / 2 - 1] % MOD + M[n / 2] % MOD) % MOD;
+		}
+	}
+}
+
+int main() {
+	ll N;
+	cin >> N;
+	cout << fib(N) % MOD << '\n';
+}
