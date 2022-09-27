91938da248adc3678fc27344ac6692705a94a3e8
oncsr <khj20006@naver.com>
2022-09-27 20:55:05 +0900

    [Gold V] Title: 포스택, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/25556. 포스택/포스택.cc
+++ 백준/Gold/25556. 포스택/포스택.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
@@ -7,20 +6,20 @@ int main() {
 	int n, a;
 	bool suc = true;
 	cin >> n;
-	vector<vector<int> > V(4);
+	int V[4]{};
 	for (int i = 0; i < n; i++) {
 		cin >> a;
-		if (V[0].empty())	V[0].push_back(a);
-		else if(a > V[0][V[0].size() - 1]) V[0].push_back(a);
+		if (!V[0])	V[0] = a;
+		else if (a > V[0]) V[0] = a;
 		else {
-			if (V[1].empty())	V[1].push_back(a);
-			else if (a > V[1][V[1].size() - 1])	V[1].push_back(a);
+			if (!V[1])	V[1] = a;
+			else if (a > V[1])	V[1] = a;
 			else {
-				if (V[2].empty())	V[2].push_back(a);
-				else if (a > V[2][V[2].size() - 1])	V[2].push_back(a);
+				if (!V[2])	V[2] = a;
+				else if (a > V[2])	V[2] = a;
 				else {
-					if (V[3].empty())	V[3].push_back(a);
-					else if (a > V[3][V[3].size() - 1])	V[3].push_back(a);
+					if (!V[3])	V[3] = a;
+					else if (a > V[3])	V[3] = a;
 					else {
 						suc = false;
 						break;
