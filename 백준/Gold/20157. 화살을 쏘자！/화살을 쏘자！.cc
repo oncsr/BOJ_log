69639c3fdcac166b000397cd6cf17940e315e531
oncsr <khj20006@naver.com>
2022-09-30 22:01:09 +0900

    [Gold V] Title: 화살을 쏘자!, Time: 92 ms, Memory: 8360 KB -BaekjoonHub

--- 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.cc
+++ 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.cc
@@ -6,41 +6,29 @@ int gcd(int a, int b) {
 	return a % b ? gcd(b, a % b) : b;
 }
 int main() {
-	int n, x, y, m = 0;
+	cin.tie(0)->sync_with_stdio(0);
+	int n, x, y, m = 0, p, q;
 	map<pair<int, int>, int> M;
 	cin >> n;
 	while (n--) {
 		cin >> x >> y;
 		if (y == 0) {
-			if (x > 0) {
-				if (!M[{0, 1}])	M[{0, 1}] = 1;
-				else	M[{0, 1}]++;
-				m = max(M[{0, 1}], m);
-			}
-			else {
-				if (!M[{0, -1}])	M[{0, -1}] = 1;
-				else	M[{0, -1}]++;
-				m = max(M[{0, -1}], m);
-			}
+			if (x > 0)	p = 1;
+			else	p = -1;
+			q = 0;
 		}
 		else if (x == 0) {
-			if (y > 0) {
-				if (!M[{1, 0}])	M[{1, 0}] = 1;
-				else	M[{1, 0}]++;
-				m = max(M[{1, 0}], m);
-			}
-			else {
-				if (!M[{-1, 0}])	M[{-1, 0}] = 1;
-				else	M[{-1, 0}]++;
-				m = max(M[{-1, 0}], m);
-			}
+			if (y > 0)	q = 1;
+			else	q = -1;
+			p = 0;
 		}
 		else {
 			int c = gcd(abs(x), abs(y));
-			if (!M[{x / c, y / c}])	M[{x / c, y / c}] = 1;
-			else	M[{x / c, y / c}]++;
-			m = max(m, M[{x / c, y / c}]);
+			p = x / c, q = y / c;
 		}
+		if (!M[{p, q}])	M[{p, q}] = 1;
+		else	M[{p, q}]++;
+		m = max(m, M[{p, q}]);
 	}
 	cout << m;
 }
