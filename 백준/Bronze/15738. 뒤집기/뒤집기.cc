9bed4044f4431764be2f284dfc68621d84f2aef6
oncsr <khj20006@naver.com>
2022-10-04 04:51:02 +0900

    [Bronze I] Title: 뒤집기, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15738. 뒤집기/뒤집기.cc
+++ 백준/Bronze/15738. 뒤집기/뒤집기.cc
@@ -1,15 +1,3 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-	int n, k, m, a, g, c;
-	cin >> n >> k >> m;
-	c = k;
-	for (g = n; g--;)cin >> a;
-	while (m--) {
-		cin >> a;
-		if (a > 0 && c <= a)	c = (a + 1) - c;
-		else if (a < 0 && c >= n + a + 1)	c = (2 * n + a + 1) - c;
-	}
-	cout << c;
-}
+main(){int n,k,m,a,g,c;cin>>n>>k>>m;c=k;for(g=n;g--;)cin>>a;while(m--){cin>>a;if (a>0&&c<=a)c=(a+1)-c;else if(a<0&&c>=n+a+1)c=(2*n+a+1)-c;}cout<<c;}
