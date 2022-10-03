cdbe4bdfe6ee8a53bdc6b6c32fab601e1c42190a
oncsr <khj20006@naver.com>
2022-10-04 06:44:11 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.cc
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -1,3 +1,3 @@
 #include<iostream>
 #include<algorithm>
-main(){int n,a[1000],l;std::cin>>n>>l;for(int i=0;i<n;i++)std::cin>>a[i];std::sort(a,a+n);for(int i=0;i<n;i++)if(a[i]<=l)l++;std::cout<<l;}
+main(){int n,a[1000],i,l;std::cin>>n>>l;for(i=0;i<n;i++)std::cin>>a[i];std::sort(a,a+n);for(i=0;i<n;i++)if(a[i]<=l)l++;std::cout<<l;}
