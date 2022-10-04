cf364f578e9bb386aff5c11bba00ebf1526441d9
oncsr <khj20006@naver.com>
2022-10-05 01:13:03 +0900

    [Platinum V] Title: 공장, Time: 280 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int arr[1000001]{}, b[1000001]{};
+int seg[1048577]{};
+
+int find(int s, int e, int l, int r, int n) {
+	if (l > e || r < s)	return 0;
+	if (l <= s && e <= r)	return seg[n];
+	int m = (s + e) / 2;
+	return find(s, m, l, r, n * 2) + find(m + 1, e, l, r, n * 2 + 1);
+}
+
+void update(int s, int e, int p, int n) {
+	if (s == e) {
+		seg[n] = 1;
+		return;
+	}
+	int m = (s + e) / 2;
+	if (p <= m)	update(s, m, p, n * 2);
+	else	update(m + 1, e, p, n * 2 + 1);
+	seg[n] = seg[n * 2] + seg[n * 2 + 1];
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, a;
+	ll s = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i++)	cin >> arr[i];
+	for (int i = 1; i <= N; i++) {
+		cin >> a;
+		b[a] = i;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int g = b[arr[i]];
+		s += (ll)find(1, N, g + 1, N, 1);
+		update(1, N, g, 1);
+	}
+	cout << s;
+}
