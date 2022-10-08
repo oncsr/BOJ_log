9598e4e58415683216a4d9cfe82b0f5527aa1bce
oncsr <khj20006@naver.com>
2022-10-09 03:20:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2420 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main() {
+	int N, K;
+	cin >> N >> K;
+	int visit[100001] = { 0 };
+	queue<pair<int, int> > Q;
+	Q.push({ N,0 });
+	visit[N]++;
+	int time = 0;
+	while (!Q.empty()) {
+		int c = Q.front().first;
+		time = Q.front().second;
+		Q.pop();
+		if (c == K)
+			break;
+		int xx = c + 1;
+		int zz = c - 1;
+		int yy = 2 * c;
+		if (yy <= 100000 && yy >= 0 && visit[yy] == 0) {
+			Q.push({ yy,time });
+			visit[yy]++;
+		}
+		if (zz <= 100000 && zz >= 0 && visit[zz] == 0) {
+			Q.push({ zz,time + 1 });
+			visit[zz]++;
+		}
+		if (xx <= 100000 && xx >= 0 && visit[xx] == 0) {
+			Q.push({ xx,time + 1 });
+			visit[xx]++;
+		}
+	}
+	cout << time << '\n';
+}
