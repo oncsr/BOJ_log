3c8e61eb4942d0e6e2d03a7030656e85fba3f465
oncsr <khj20006@naver.com>
2022-10-09 16:05:29 +0900

    [Gold V] Title: 트리와 쿼리, Time: 60 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -1,30 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int s[100001]{}, p[100001]{};
-vector<vector<int> > V(100001);
-void dfs(int n, int P) {
-	p[n] = P, s[n] = 1;
-	for (int i : V[n])
-		if (i != P) {
-			dfs(i, n);
-			s[n] += s[i];
-		}
-}
-
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	int N, R, Q, a, b;
-	cin >> N >> R >> Q;
-	for (int i = 1; i < N; i++) {
-		cin >> a >> b;
-		V[a].push_back(b);
-		V[b].push_back(a);
-	}
-	dfs(R, 0);
-	for (; Q--;) {
-		cin >> a;
-		cout << s[a] << '\n';
-	}
-}
+#include<bits/stdc++.h>
+using namespace std;int s[100001][2];vector<int>V[100001];void d(int n,int P){s[n][1]=P,s[n][0]=1;for (int i:V[n])if(i!=P){d(i,n);s[n][0]+=s[i][0];}}
+main(){cin.tie(0)->sync_with_stdio(0);int N,R,Q,a,b;cin>>N>>R>>Q;for (int i=1;i<N;i++){cin>>a>>b;V[a].push_back(b);V[b].push_back(a);}d(R,0);for(;Q--;){cin>>a;cout<<s[a][0] << '\n'; } }
