dee4f09ccfd58922ee9f92009349ed8f866823e1
oncsr <khj20006@naver.com>
2022-10-13 13:19:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 84 ms, Memory: 9108 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+bool cmp(int a, int b) { return a > b; }
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, M, R, a, b, c = 0;
+	cin >> N >> M >> R;
+	vector<vector<int> > V(100001);
+	queue<int> Q;
+	int visit[100001]{}, ord[100001]{};
+	for (; M--;) {
+		cin >> a >> b;
+		V[a].push_back(b);
+		V[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++)	sort(V[i].begin(), V[i].end(), cmp);
+	Q.push(R);
+	visit[R]++;
+	while (!Q.empty()) {
+		int x = Q.front();
+		ord[x] = ++c;
+		Q.pop();
+		for (int i : V[x])
+			if (!visit[i]) {
+				Q.push(i);
+				visit[i]++;
+			}
+	}
+	for (int i = 1; i <= N; i++)	cout << ord[i] << '\n';
+}
