e8a9e02a7d5264735cf6395865217715815f5480
oncsr <khj20006@naver.com>
2022-10-19 17:15:42 +0900

    [Silver V] Title: 숫자 카드, Time: 196 ms, Memory: 21432 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,31 +1,16 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int a[500001];
-
-int bin(int s, int e, int v) {
-	int m = (s + e) / 2;
-	if (s > e)	return 0;
-	if (s == e) {
-		if (v == a[m])	return 1;
-		else	return 0;
-	}
-	else {
-		if (v > a[m])	return bin(m + 1, e, v);
-		else if (v == a[m])	return 1;
-		else	return bin(s, m - 1, v);
-	}
-}
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	int n, m, b;
-	cin >> n;
-	for (int i = 0; i < n; i++)	cin >> a[i];
-	sort(a, a + n);
-	cin >> m;
-	while (m--) {
-		cin >> b;
-		cout << bin(0, n - 1, b) << ' ';
+	int N, M, a;
+	bool arr[20000001]{};
+	for (cin >> N; N--;) {
+		cin >> a;
+		arr[a + 10000000] |= true;
+	}
+	for (cin >> M; M--;) {
+		cin >> a;
+		cout << (arr[a + 10000000] ? 1 : 0) << ' ';
 	}
 }
