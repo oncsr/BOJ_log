c11fec06b98caf6bb4bde9c3a2057acb68aeec2e
oncsr <khj20006@naver.com>
2022-11-01 18:34:31 +0900

    [Silver III] Title: 정사각형, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1485. 정사각형/정사각형.cc
+++ 백준/Silver/1485. 정사각형/정사각형.cc
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-using ll = int;
 
-ll R(pair<ll, ll> a, pair<ll, ll> b) {
+int R(pair<int, int> a, pair<int, int> b) {
 	return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
 }
 
 int main() {
 	int N;
 	for (cin >> N; N--;) {
-		pair<ll, ll> P[4]{};
+		pair<int, int> P[4]{};
 		for (int i = 0; i < 4; i++)	cin >> P[i].first >> P[i].second;
 		sort(P, P + 4);
-		ll d1 = R(P[0], P[1]);
-		ll d2 = R(P[0], P[2]);
-		ll d3 = R(P[1], P[3]);
-		ll d4 = R(P[2], P[3]);
+		int d1 = R(P[0], P[1]);
+		int d2 = R(P[0], P[2]);
+		int d3 = R(P[1], P[3]);
+		int d4 = R(P[2], P[3]);
 		bool c = (d1 == d2 && d2 == d3 && d3 == d4);
 		if (P[0].first == P[1].first) {
 			bool c1 = P[2].first == P[3].first;
@@ -25,8 +24,8 @@ int main() {
 			c &= (c1 & c2 & c3);
 		}
 		else {
-			ll d5 = R(P[0], P[3]);
-			ll d6 = R(P[1], P[2]);
+			int d5 = R(P[0], P[3]);
+			int d6 = R(P[1], P[2]);
 			bool c1 = (d5 == d6);
 			c &= c1;
 		}
