8e1940ccae7ce97879c28f664c55009872fb5275
oncsr <khj20006@naver.com>
2022-11-11 11:37:16 +0900

    [Platinum V] Title: 합과 곱, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Platinum/1353. 합과 곱/합과 곱.cc
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double S, P;
+	cin >> S >> P;
+	int find = 0;
+	double temp = S, g;
+	if (P < S)	cout << 2;
+	else if (S == P) cout << 1;
+	else if (temp >= P)	cout << 1;
+	else {
+		for (int i = 1; i <= 30; i++) {
+			if (pow(S / i, i) >= P) {
+				find = 1;
+				cout << i;
+				break;
+			}
+		}
+		if (!find)	cout << -1;
+	}
+}
