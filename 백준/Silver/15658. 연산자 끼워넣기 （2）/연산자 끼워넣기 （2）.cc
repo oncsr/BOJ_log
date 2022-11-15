d3041c85b9ab906bac9fe6f206a59722b485e499
oncsr <khj20006@naver.com>
2022-11-15 19:44:46 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/*
+* 2022.11.13
+* [used algorithm]
+* => backtracking
+* [point]
+* => 쓸 수 있는 연산자를 하나씩 다 써서 모든 케이스를 확인
+*/
+int N, arr[11]{}, op[4]{}, mn = 1000000001, mx = -1000000001;
+
+void R(int n, int res) {
+	if (n == N) {
+		mx = max(mx, res);
+		mn = min(mn, res);
+		return;
+	}
+	if (op[0]) {
+		op[0]--;
+		R(n + 1, res + arr[n]);
+		op[0]++;
+	}
+	if (op[1]) {
+		op[1]--;
+		R(n + 1, res - arr[n]);
+		op[1]++;
+	}
+	if (op[2]) {
+		op[2]--;
+		R(n + 1, res * arr[n]);
+		op[2]++;
+	}
+	if (op[3]) {
+		op[3]--;
+		R(n + 1, res / arr[n]);
+		op[3]++;
+	}
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++)	cin >> arr[i];
+	for (int i = 0; i < 4; i++)	cin >> op[i];
+	R(1, arr[0]);
+	cout << mx << '\n' << mn;
+}
