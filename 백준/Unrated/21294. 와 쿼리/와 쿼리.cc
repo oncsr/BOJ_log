9be7661d220e11f2e97284e8074154a3db9f845f
oncsr <khj20006@naver.com>
2022-11-23 20:41:05 +0900

    [Unrated] Title: 와 쿼리, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Unrated/21294. 와 쿼리/와 쿼리.cc
+++ 백준/Unrated/21294. 와 쿼리/와 쿼리.cc
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	int Q, a;
-	for (cin >> Q; Q--;) {
-		cin >> a;
-		if (a == 2)	cout << "\n";
+	int Q,a;
+	for (std::cin>>Q;Q--;) {
+		std::cin>>a;
+		if(a==2)std::cout<<'\n';
 	}
 }
