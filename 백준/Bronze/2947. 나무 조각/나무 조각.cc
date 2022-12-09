d5c7359fa6b8e5e88d410fc67496d566cd1b3224
oncsr <khj20006@naver.com>
2022-12-09 19:35:07 +0900

    [Bronze I] Title: 나무 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[5]{};
+	for (int i = 0; i < 5; i++)	cin >> a[i];
+	for (int i = 4; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
+				for (int k = 0; k < 5; k++)	cout << a[k] << ' ';
+				cout << '\n';
+			}
+		}
+	}
+}
