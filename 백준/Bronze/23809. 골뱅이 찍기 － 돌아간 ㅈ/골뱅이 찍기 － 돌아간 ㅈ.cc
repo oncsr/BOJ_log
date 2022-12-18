d634aaf60252ab5ae13f8cf410670b7b91fc3779
oncsr <khj20006@naver.com>
2022-12-18 20:58:55 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n * 3; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n * 2; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 3; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n * 2; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n * 3; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+}
