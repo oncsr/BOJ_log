858b23af25c8ecfb7decde10e1aaeb1f02c567cf
oncsr <khj20006@naver.com>
2022-12-18 20:55:56 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 3; j++)	cout << '@';
+		for (int j = 0; j < n; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n * 3; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n; j++)	cout << ' ';
+		for (int j = 0; j < n; j++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)	cout << '@';
+		for (int j = 0; j < n; j++)	cout << ' ';
+		for (int j = 0; j < n * 3; j++)	cout << '@';
+		cout << '\n';
+	}
+}
