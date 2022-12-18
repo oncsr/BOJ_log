d1e6ac82c3440eadd65d286b0b09a206d8fe7bd7
oncsr <khj20006@naver.com>
2022-12-18 20:53:32 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;	cin >> n;
+	for (int j = n; j < n * 5; j++) {
+		for (int i = 0; i < n; i++)	cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 5; j++)	cout << '@';
+		cout << '\n';
+	}
+}
