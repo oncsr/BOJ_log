e1124028e82957a36eef09a09fec6a6acd90ba1f
oncsr <khj20006@naver.com>
2023-02-01 04:19:21 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -5,14 +5,15 @@ int main(){
     cin.tie(0)->sync_with_stdio(0);
     int T;
     for(cin>>T;T--;){
-        int N, d[100002][2]{};
+        int N, d[100002][2]{}, mx = 0;
         cin>>N;
         for(int i=2;i<N+2;i++)  cin>>d[i][0];
-        for(int i=2;i<N+2;i++)  cin>>d[i][1];
-        for(int i=2;i<N+2;i++){
-            d[i][0] = max({d[i-1][1], d[i-2][0], d[i-2][1]}) + d[i][0];
-            d[i][1] = max({d[i-1][0], d[i-2][0], d[i-2][1]}) + d[i][1];
+        for(int i=2;i<N+2;i++) {
+            cin>>d[i][1];
+            d[i][0] += max({d[i-1][1], d[i-2][0], d[i-2][1]});
+            d[i][1] += max({d[i-1][0], d[i-2][0], d[i-2][1]});
+            mx = max({mx, d[i][0], d[i][1]});
         }
-        cout<<max({d[N][0], d[N][1], d[N+1][0], d[N+1][1]})<<'\n';
+        cout<<mx<<'\n';
     }
 }
