1e46a814afe8ebb1c9a6c2b4137f58c48571b647
oncsr <khj20006@naver.com>
2023-02-14 21:03:35 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 172 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -2,32 +2,32 @@
 using namespace std;
 using ii = pair<int, int>;
 
-// 현재 위치가 now이고 크기가 n인 하노이 탑을 dest에 놓는 함수
 vector<ii> ans;
 
 void Hanoi(int n, int now, int dest) {
 	if (n == 1) {
-		ans.push_back({ now, dest });
+		cout << now << ' ' << dest << '\n';
 		return;
 	}
+
 	if (now == 1) {
 		if (dest == 2)	Hanoi(n - 1, now, 3);
 		else	Hanoi(n - 1, now, 2);
-		ans.push_back({ now, dest });
+		cout << now << ' ' << dest << '\n';
 		if (dest == 2)	Hanoi(n - 1, 3, 2);
 		else	Hanoi(n - 1, 2, 3);
 	}
 	else if (now == 2) {
 		if (dest == 1)	Hanoi(n - 1, now, 3);
 		else	Hanoi(n - 1, now, 1);
-		ans.push_back({ now, dest });
+		cout << now << ' ' << dest << '\n';
 		if (dest == 1)	Hanoi(n - 1, 3, 1);
 		else	Hanoi(n - 1, 1, 3);
 	}
 	else {
 		if (dest == 1)	Hanoi(n - 1, now, 2);
 		else	Hanoi(n - 1, now, 1);
-		ans.push_back({ now, dest });
+		cout << now << ' ' << dest << '\n';
 		if (dest == 1)	Hanoi(n - 1, 2, 1);
 		else	Hanoi(n - 1, 1, 2);
 	}
@@ -36,8 +36,6 @@ void Hanoi(int n, int now, int dest) {
 int main() {
 	int N;
 	cin >> N;
+	cout << ((1 << N) - 1) << '\n';
 	Hanoi(N, 1, 3);
-	cout << ans.size() << '\n';
-	for (ii cur : ans)
-		cout << cur.first << ' ' << cur.second << '\n';
 }
