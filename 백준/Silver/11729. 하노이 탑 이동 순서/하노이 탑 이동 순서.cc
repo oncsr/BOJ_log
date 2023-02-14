d49255e52c175c790b19e3e6404c7864f377781b
oncsr <khj20006@naver.com>
2023-02-14 21:08:13 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 168 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,8 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ii = pair<int, int>;
-
-vector<ii> ans;
 
 void Hanoi(int n, int now, int dest) {
 	if (n == 1) {
@@ -10,27 +7,11 @@ void Hanoi(int n, int now, int dest) {
 		return;
 	}
 
-	if (now == 1) {
-		if (dest == 2)	Hanoi(n - 1, now, 3);
-		else	Hanoi(n - 1, now, 2);
-		cout << now << ' ' << dest << '\n';
-		if (dest == 2)	Hanoi(n - 1, 3, 2);
-		else	Hanoi(n - 1, 2, 3);
-	}
-	else if (now == 2) {
-		if (dest == 1)	Hanoi(n - 1, now, 3);
-		else	Hanoi(n - 1, now, 1);
-		cout << now << ' ' << dest << '\n';
-		if (dest == 1)	Hanoi(n - 1, 3, 1);
-		else	Hanoi(n - 1, 1, 3);
-	}
-	else {
-		if (dest == 1)	Hanoi(n - 1, now, 2);
-		else	Hanoi(n - 1, now, 1);
-		cout << now << ' ' << dest << '\n';
-		if (dest == 1)	Hanoi(n - 1, 2, 1);
-		else	Hanoi(n - 1, 1, 2);
-	}
+	int other = 6 - (now + dest);
+
+	Hanoi(n - 1, now, other);
+	cout << now << ' ' << dest << '\n';
+	Hanoi(n - 1, other, dest);
 }
 
 int main() {
