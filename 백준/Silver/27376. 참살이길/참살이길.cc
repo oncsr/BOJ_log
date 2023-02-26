b39c52c92ac311769065238c87186550ec025ff9
oncsr <khj20006@naver.com>
2023-02-26 22:36:15 +0900

    [Silver IV] Title: 참살이길, Time: 184 ms, Memory: 6644 KB -BaekjoonHub

---
+++ 백준/Silver/27376. 참살이길/참살이길.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct sinho {
+	ll x, t, s;
+};
+
+bool cmp(sinho a, sinho b) { return a.x < b.x; }
+
+int main() {
+	ll n, k;
+	cin >> n >> k;
+	vector<sinho> V;
+	for (; k--;) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		V.push_back({ a,b,c });
+	}
+	sort(V.begin(), V.end(), cmp);
+	ll ans = 0, pos = 0;
+	for (sinho now : V) {
+		ans += (now.x - pos);
+		pos = now.x;
+		if (ans < now.s)	ans = now.s;
+		else if (((ans - now.s) / now.t) % 2)
+			ans = now.s + (((ans - now.s) / now.t) + 1) * now.t;
+	}
+	ans += (n - pos);
+	cout << ans;
+}
