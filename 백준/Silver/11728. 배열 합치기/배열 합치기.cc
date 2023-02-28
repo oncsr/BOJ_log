f70696c3fba20056b2b8ae29ea20d4c386fb0183
oncsr <khj20006@naver.com>
2023-02-28 23:56:56 +0900

    [Silver V] Title: 배열 합치기, Time: 508 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
+	cout.tie(0);
 	int n, m, a, arr[1000000]{}, id = 0;
 	cin >> n >> m;
 	for (int i = 0; i < n; i++)	cin >> arr[i];
