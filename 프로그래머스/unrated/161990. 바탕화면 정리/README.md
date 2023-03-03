0c82dac8fb7d9956c2ed2c1acbdfc5f3236e3046
oncsr <khj20006@naver.com>
2023-03-03 13:27:14 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/README.md
+++ 프로그래머스/unrated/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [unrated] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -4,24 +4,22 @@
 using namespace std;
 
 vector<int> solution(vector<string> wallpaper) {
-	vector<int> answer;
 	
-	pair<int, int> one = { wallpaper.size(), wallpaper[0].size() };
-	pair<int, int> two = { 0,0 };
+	int a = wallpaper.size();
+	int b = wallpaper[0].size();
+	int c = 0, d = 0;
 
 	for (int i = 0; i < wallpaper.size(); i++) {
 		for (int j = 0; j < wallpaper[i].size(); j++) {
 			if (wallpaper[i][j] == '#') {
-				one.first = min(one.first, i);
-				one.second = min(one.second, j);
-				two.first = max(two.first, i + 1);
-				two.second = max(two.second, j + 1);
+				a = min(a, i);
+				b = min(b, j);
+				c = max(c, i + 1);
+				d = max(d, j + 1);
 			}
 		}
 	}
-	answer.push_back(one.first);
-	answer.push_back(one.second);
-	answer.push_back(two.first);
-	answer.push_back(two.second);
+	
+	vector<int> answer = { a,b,c,d };
 	return answer;
 }
