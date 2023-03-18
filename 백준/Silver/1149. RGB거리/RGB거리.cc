d46e5cba9a636f8c8a1f037d7c3983ad7022f256
oncsr <khj20006@naver.com>
2023-03-18 16:46:22 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -2,16 +2,14 @@
 using namespace std;
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);	cout.tie(0);
-	int n;
-	cin >> n;
-	int d[1001][3]{};
-	for (int i = 1; i <= n; i++) {
-		cin >> d[i][0] >> d[i][1] >> d[i][2];
-		if (i == 1)	continue;
-		d[i][0] += min(d[i - 1][1], d[i - 1][2]);
-		d[i][1] += min(d[i - 1][0], d[i - 1][2]);
-		d[i][2] += min(d[i - 1][0], d[i - 1][1]);
+	cin.tie(0)->sync_with_stdio(0);
+	int N, arr[1001][3]{};
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+		arr[i][0] += min(arr[i - 1][1], arr[i - 1][2]);
+		arr[i][1] += min(arr[i - 1][0], arr[i - 1][2]);
+		arr[i][2] += min(arr[i - 1][0], arr[i - 1][1]);
 	}
-	cout << min(d[n][0], min(d[n][1], d[n][2]));
+	cout << min(arr[N][0], min(arr[N][1], arr[N][2]));
 }
