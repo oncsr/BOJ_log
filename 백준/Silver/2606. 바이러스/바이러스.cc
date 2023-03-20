acf33fdcc2e3219e16ef8b0206061183ce306d6b
oncsr <khj20006@naver.com>
2023-03-20 22:24:24 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,38 +1,30 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
-int com[101][101];
-int visit[101] = { 0 };
-int n;
-int v;
-queue<int> Q;
 
-int bfs(int start) {
-	int cnt = 0;
-	Q.push(start);
-	visit[start] = 1;
-	while (!Q.empty()) {
-		int c = Q.front();
-		Q.pop();
-		cnt++;
-		for (int i = 1; i <= n; i++) {
-			if (!visit[i] && com[c][i] == 1) {
-				Q.push(i);
-				visit[i] = 1;
-			}
+
+vector<vector<int> > V(101);
+int visit[101]{}, N, M, a, b, ans = 0;
+void dfs(int n, int p) {
+	for (int i : V[n]) {
+		if (i != p && !visit[i]) {
+			visit[i]++;
+			ans++;
+			dfs(i, n);
 		}
 	}
-	return cnt;
 }
 
 int main() {
-	cin >> n;
-	cin >> v;
-	for (int i = 0; i < v; i++) {
-		int a, b;
+	cin.tie(0)->sync_with_stdio(0);
+
+	for (cin >> N >> M; M--;) {
 		cin >> a >> b;
-		com[a][b] = 1;
-		com[b][a] = 1;
+		V[a].push_back(b);
+		V[b].push_back(a);
 	}
-	cout << bfs(1) - 1 << '\n';
+	visit[1]++;
+	dfs(1, 0);
+	cout << ans;
+	
 }
