db3f444ebb65fdab0bb00a45a41e4cdcaafb3637
oncsr <khj20006@naver.com>
2023-03-21 20:40:53 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -3,22 +3,17 @@
 using namespace std;
 using ii = pair<int, int>;
 
-bool cmp(ii a, ii b) {
-	if (a.second == b.second)	return a.first < b.first;
-	return a.second < b.second;
-}
-
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	int N, s = 0;
 	ii arr[100000]{};
 	cin >> N;
-	for (int i = 0; i < N; i++)	cin >> arr[i].first >> arr[i].second;
-	sort(arr, arr + N, cmp);
+	for (int i = 0; i < N; i++)	cin >> arr[i].second >> arr[i].first;
+	sort(arr, arr + N);
 	int ans = 1;
-	ii now = arr[0];
+	int now = arr[0].first;
 	for (int i = 1; i < N; i++) {
-		if (arr[i].first >= now.second)	ans++, now = arr[i];
+		if (arr[i].second >= now)	ans++, now = arr[i].first;
 	}
 	cout << ans;
 }
