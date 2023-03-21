e36092cb3fe029093ea784603cbb524216f905d2
oncsr <khj20006@naver.com>
2023-03-21 21:38:02 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main() {
-	int N, d[1000001] = { 0,1,2 };
+	int N, s = 2, b = 1, a = 1;
 	cin >> N;
-	for (int i = 3; i <= N; i++)
-		d[i] = (d[i - 1] + d[i - 2]) % 15746;
-	cout << d[N];
+	if (N == 1) {
+		cout << 1;
+		return 0;
+	}
+	for (int i = 3; i <= N; i++) {
+		int temp = b;
+		b = s;
+		a = temp;
+		s = (a + b) % 15746;
+	}
+		
+	cout << s;
 }
