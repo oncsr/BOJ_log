3653522c83393f8479c7a6fec0c598ea103f25ac
oncsr <khj20006@naver.com>
2023-03-21 21:36:04 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -2,11 +2,9 @@
 using namespace std;
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);	cout.tie(0);
-	int n, mod = 15746;
-	int d[1000001]{ 1 };
-	cin >> n;
-	d[1] = 1;
-	for (int i = 2; i <= n; i++)	d[i] = (d[i - 1] % mod + d[i - 2] % mod) % mod;
-	cout << d[n];
+	int N, d[1000001] = { 0,1,2 };
+	cin >> N;
+	for (int i = 3; i <= N; i++)
+		d[i] = (d[i - 1] + d[i - 2]) % 15746;
+	cout << d[N];
 }
