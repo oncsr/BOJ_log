e3805c9d4aded45807ab54881cb7a69abdc3bd28
oncsr <khj20006@naver.com>
2023-03-22 22:49:07 +0900

    [Silver IV] Title: 수 찾기, Time: 68 ms, Memory: 8132 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,44 +1,17 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-vector<int> v;
-
-int binary_search(int start, int end, int num) {
-	int mid = (end - start) / 2;
-	if (end == start) {
-		if (v[end] == num)
-			return 1;
-		else
-			return 0;
-	}
-	else {
-		if (v[start+mid] == num)
-			return 1;
-		else if (v[start+mid] > num)
-			return binary_search(start, end - mid-1, num);
-		else
-			return binary_search(start + mid + 1, end, num);
-	}
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int a;
+	cin.tie(0)->sync_with_stdio(0);
+	unordered_map<int, bool> M;
+	int n, m, a;
+	for (cin >> n; n--;) {
 		cin >> a;
-		v.push_back(a);
+		M[a] = true;
 	}
-	sort(v.begin(), v.end());
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		int b;
-		cin >> b;
-		cout << binary_search(0, v.size()-1, b) << '\n';
+	for (cin >> m; m--;) {
+		cin >> a;
+		cout << (int)M[a] << '\n';
 	}
 }
