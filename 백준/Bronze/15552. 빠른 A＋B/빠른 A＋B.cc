5cb07f10dbee4c6dc5d3d3ca5627282a3739c480
oncsr <khj20006@naver.com>
2023-03-26 23:36:14 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 228 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int t;	cin >> t;
+	for(int i=0;i<t;i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << a+b << '\n';
+	}
+}
