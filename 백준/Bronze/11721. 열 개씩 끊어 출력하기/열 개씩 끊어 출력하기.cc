ee284f2b49b8eb1c7647da84ae5a2dc62dd18eb2
oncsr <khj20006@naver.com>
2023-03-27 01:39:58 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a;
+	cin >> a;
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i];
+		if (i % 10 == 9)
+			cout << '\n';
+	}
+}
