58a80e8dfcd491f36ea4212cf9c904620f455ac1
oncsr <khj20006@naver.com>
2023-03-28 16:35:24 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 188 ms, Memory: 26900 KB -BaekjoonHub

---
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, Q;
+	int dp[32][200001]{};
+	cin >> N;
+	for (int i = 1; i <= N; i++)	cin >> dp[0][i];
+	for (int i = 1; i <= 31; i++)
+		for (int j = 1; j <= N; j++)
+			dp[i][j] = dp[i - 1][dp[i - 1][j]];
+	for (cin >> Q; Q--;) {
+		int a, b;
+		cin >> a >> b;
+		int ans = b, g = 0;
+		while (a > 0) {
+			if (a % 2)	ans = dp[g][ans];
+			a >>= 1;
+			g++;
+		}
+		cout << ans << '\n';
+	}
+}
