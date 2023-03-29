f9f1f8716f989deadd0be3b9e851eb1d8ada204b
oncsr <khj20006@naver.com>
2023-03-29 14:14:15 +0900

    [Gold V] Title: 가희와 btd5, Time: 204 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/22238. 가희와 btd5/가희와 btd5.cc
+++ 백준/Gold/22238. 가희와 btd5/가희와 btd5.cc
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <queue>
-#include <map>
 using namespace std;
 using ll = long long;
+
+/*
+ < 2022.03.29 >
+ [used algorithm]
+ > euclidean algorithm, map, priority queue, simulation
+ [point]
+
+*/
 
 ll gcd(ll a, ll b) {
 	if (a < b)	swap(a, b);
@@ -12,31 +19,32 @@ ll gcd(ll a, ll b) {
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	map<pair<pair<ll, ll>, bool>, pair<priority_queue<ll, vector<ll>, greater<> >, ll> > balloon;
 	int N, M;
 	cin >> N >> M;
+	ll x, y;
+	vector<ll> V;
 	for (int i = 0; i < N; i++) {
 		ll a, b, c;
 		cin >> a >> b >> c;
-		bool dir = true;
-		if (a < 0)	dir = false;
-		if (a != 0 && b != 0) {
-			ll g = gcd(abs(a), abs(b));
-			a /= g, b /= g;
+		V.push_back(c);
+		if (!i) {
+			if (a != 0 && b != 0) {
+				ll g = gcd(abs(a), abs(b));
+				a /= g, b /= g;
+			}
+			else {
+				if (a == 0)	b = (b > 0 ? 1 : -1);
+				else	a = (a > 0 ? 1 : -1);
+			}
+			x = a, y = b;
 		}
-		else {
-			if (a == 0)	b = (b > 0 ? 1 : -1);
-			else	a = (a > 0 ? 1 : -1);
-		}
-		if (balloon[{ {a, b}, dir}].first.size() == 0)
-			balloon[{ {a, b}, dir}].second = 0;
-		balloon[{ {a, b}, dir}].first.push(c);
 	}
+	sort(V.begin(), V.end());
+	ll s = 0;
+	int id = 0;
 	for (int i = 0; i < M; i++) {
 		ll a, b, c;
 		cin >> a >> b >> c;
-		bool dir = true;
-		if (a < 0)	dir = false;
 		if (a != 0 && b != 0) {
 			ll g = gcd(abs(a), abs(b));
 			a /= g, b /= g;
@@ -45,18 +53,13 @@ int main() {
 			if (a == 0)	b = (b > 0 ? 1 : -1);
 			else	a = (a > 0 ? 1 : -1);
 		}
-		if (balloon[{ {a, b}, dir}].first.size() == 0) {
-			cout << N << '\n';
+		if (!(x == a && y == b)) {
+			cout << N - id << '\n';
 			continue;
 		}
-		balloon[{ {a, b}, dir}].second += c;
-		while (!balloon[{ {a, b}, dir}].first.empty()) {
-			if (balloon[{ {a, b}, dir}].first.top() <= balloon[{ {a, b}, dir}].second) {
-				balloon[{ {a, b}, dir}].first.pop();
-				N--;
-			}
-			else	break;
-		}
-		cout << N << '\n';
+		s += c;
+		while (id < N && V[id] <= s)	id++;
+		cout << N - id << '\n';
 	}
+	
 }
