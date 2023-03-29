b6d83e905b686c9877e632b154c2d9e0176dba48
oncsr <khj20006@naver.com>
2023-03-29 14:34:49 +0900

    [Gold II] Title: 차이를 최대로 2, Time: 176 ms, Memory: 17524 KB -BaekjoonHub

--- 백준/Gold/16193. 차이를 최대로 2/차이를 최대로 2.cc
+++ 백준/Gold/16193. 차이를 최대로 2/차이를 최대로 2.cc
@@ -3,44 +3,40 @@
 using namespace std;
 using ll = long long;
 
-int N;
-ll arr[1000000]{};
-ll arr1[1000000]{};
-
-bool cmp(ll a, ll b) { return a > b; }
-
-ll task() {
-	int s = 0, e = N - 1;
-	for (int i = 0; i < N; i++) {
-		if (i % 2 == 0)	arr1[i] = arr[s++];
-		else	arr1[i] = arr[e--];
-	}
-	ll ans = 0;
-	for (int i = 1; i < N; i++)
-		ans += abs(arr1[i] - arr1[i - 1]);
-
-	ll cnt = ans;
-	for (int i = 0; i < N - 1; i++) {
-		int right = i + 1, left = (i ? i - 1 : N - 1);
-		cnt -= abs(arr1[i] - arr1[right]);
-		cnt += abs(arr1[i] - arr1[left]);
-		ans = max(ans, cnt);
-	}
-
-	return ans;
-
-}
-
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
+	int N;
+	ll arr[1000000]{};
 	cin >> N;
 	for (int i = 0; i < N; i++)	cin >> arr[i];
 	sort(arr, arr + N);
-	ll val1 = task();
-	sort(arr, arr + N, cmp);
-	ll val2 = task();
-	
-	cout << max(val1, val2);
-	
-
+	ll T[1000000]{};
+	for (int i = 0; i < N / 2; i++) {
+		T[i * 2] = arr[i];
+		T[i * 2 + 1] = arr[N - 1 - i];
+	}
+	if (N % 2)	T[N - 1] = arr[N / 2];
+	ll s = 0;
+	for (int i = 0; i < N - 1; i++)	s += abs(T[i] - T[i + 1]);
+	ll mx = s;
+	for (int i = 0; i < N - 1; i++) {
+		s -= abs(T[i] - T[i + 1]);
+		if (i == 0)	s += abs(T[i] - T[N - 1]);
+		else	s += abs(T[i] - T[i - 1]);
+		mx = max(mx, s);
+	}
+	for (int i = 0; i < N / 2; i++) {
+		T[i * 2] = arr[N - 1 - i];
+		T[i * 2 + 1] = arr[i];
+	}
+	if (N % 2)	T[N - 1] = arr[N / 2];
+	s = 0;
+	for (int i = 0; i < N - 1; i++)	s += abs(T[i] - T[i + 1]);
+	for (int i = 0; i < N - 1; i++) {
+		s -= abs(T[i] - T[i + 1]);
+		if (i == 0)	s += abs(T[i] - T[N - 1]);
+		else	s += abs(T[i] - T[i - 1]);
+		mx = max(mx, s);
+	}
+	cout << mx;
 }
