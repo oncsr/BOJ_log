6604ea39605d2184d54393f61ff84d94822ff368
oncsr <khj20006@naver.com>
2023-04-03 16:42:17 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,24 +1,62 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int arr[10000] = { 0 };
+int id = 0;
+
+void push(int ab) {
+	arr[id] = ab;
+	id++;
+}
+
+void pop() {
+	if (id == 0)
+		printf("-1\n");
+	else {
+		printf("%d\n", arr[id - 1]);
+		arr[id - 1] = 0;
+		id--;
+	}
+}
+
+void size() {
+	printf("%d\n", id);
+}
+
+void empty() {
+	if (id == 0)
+		printf("1\n");
+	else
+		printf("0\n");
+}
+
+void top() {
+	if (id == 0)
+		printf("-1\n");
+	else
+		printf("%d\n", arr[id - 1]);
+}
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	stack<int> S;
-	int N, a;
-	string o;
-	for (cin >> N; N--;) {
-		cin >> o;
-		if (o == "push")	cin >> a, S.push(a);
-		if (o == "pop") {
-			if (S.empty())	cout << "-1\n";
-			else	cout << S.top() << '\n', S.pop();
+	int num;
+	scanf("%d", &num);
+	
+	for (; num > 0; num--) {
+		char word[10];
+		scanf("%s", word);
+		if (strcmp(word, "push") == 0) {
+			int x;
+			scanf("%d", &x);
+			push(x);
 		}
-		if (o == "size")	cout << S.size() << '\n';
-		if (o == "empty")	cout << (int)S.empty() << '\n';
-		if (o == "top") {
-			if (S.empty())	cout << "-1\n";
-			else	cout << S.top() << '\n';
+		else if (strcmp(word, "pop") == 0) {
+			pop();
 		}
+		else if (strcmp(word, "size") == 0) {
+			size();
+		}
+		else if (strcmp(word, "empty") == 0)	empty();
+		else	top();
 	}
 }
