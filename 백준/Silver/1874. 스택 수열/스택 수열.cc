bd8e28d066e61e7ea63652e5f2df1c298cafa04a
oncsr <khj20006@naver.com>
2023-04-04 14:28:49 +0900

    [Silver II] Title: 스택 수열, Time: 44 ms, Memory: 3020 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,25 +1,34 @@
 #include <iostream>
-#include <vector>
 #include <stack>
 using namespace std;
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	int N, a, s = 1, now = 1;
-	stack<int> S;
-	vector<char> V;
-	for (cin >> N; N--;) {
+	int n, cur = 1, a, arr[100001]{}, no = 0;
+	cin >> n;
+	string ans = "";
+	for (int i = 0; i < n; i++) {
 		cin >> a;
-		while (s <= a) {
-			S.push(s++);
-			V.push_back('+');
+		if (a >= cur) {
+			for (int j = cur; j <= a; j++) {
+				if (!arr[j]) {
+					arr[j] = 1;
+					ans += "+\n";
+				}
+			}
 		}
-		if (S.empty() || S.top() != a) {
-			cout << "NO";
-			return 0;
+		else {
+			for (int j = cur - 1; j >= a; j--) {
+				if (arr[j] == 1 && j != a) {
+					cout << "NO";
+					no++;
+					break;
+				}
+			}
 		}
-		S.pop();
-		V.push_back('-');
+		if (no)	break;
+		cur = a;
+		ans += "-\n";
+		arr[a] = 2;
 	}
-	for (char i : V)	cout << i << '\n';
+	if (!no)	cout << ans;
 }
