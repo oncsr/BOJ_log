4ba5b832270b2c9bc7f17dd5d438a4805c202f5e
oncsr <khj20006@naver.com>
2023-04-09 14:29:50 +0900

    [Silver II] Title: 하이퍼 가지 따기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.cc
+++ 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.cc
@@ -3,21 +3,12 @@ using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	int arr[2047][11]{};
+	int arr[11]{};
 	for (int i = 0; i < 2047; i++)
-		for (int j = 0; j < 11; j++)
-			cin >> arr[i][j];
-	for (int j = 0; j < 11; j++) {
-		int a = arr[0][j], b = -1000001;
-		int cnta = 1, cntb = 0;
-		for (int i = 1; i < 2047; i++) {
-			if (arr[i][j] != a) {
-				if (cntb == 0)	b = arr[i][j];
-				cntb++;
-			}
-			else	cnta++;
-		}
-		if (cnta == 1023)	cout << a << ' ';
-		else	cout << b << ' ';
-	}
+		for (int j = 0; j < 11; j++){
+            int a;
+            cin>>a;
+            arr[j] ^= a;
+        }
+    for(int i=0;i<11;i++)    cout<<arr[i]<<' ';
 }
