d6ab119d0932d57bbf9d11c75bd0e1bcbab52503
oncsr <khj20006@naver.com>
2023-04-10 15:47:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5944 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,48 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
+int graph[1001][1001]{};
 int N, M, V;
-int graph1[1001][1001];
-int visit1[1001] = { 0 };
-int visit2[1001] = { 0 };
-vector<vector<int> > graph2(1001);
-queue<int> Q;
+int visit1[1001]{}, visit2[1001]{};
 
-/*
-void dfs1(int node) {
-	visit[node] = 1;
-	cout << node << ' ';
+void DFS(int n) {
+	cout << n << ' ';
 	for (int i = 1; i <= N; i++) {
-		if (!visit[i])
-			dfs1(i);
-	}
-}
-*/
-
-void dfs(int node) {
-	if (visit1[node] == 0) {
-		visit1[node] = 1;
-		cout << node << ' ';
-		for (int i = 0; i < graph2[node].size(); i++) {
-			if (!visit1[graph2[node][i]])
-				dfs(graph2[node][i]);
+		if (visit1[i] == 0 && graph[n][i]) {
+			visit1[i]++;
+			DFS(i);
 		}
 	}
 }
 
-void bfs(int node) {
-	Q.push(node);
-	visit2[node] = 1;
+void BFS(int n) {
+	queue<int> Q;
+	visit2[n]++;
+	Q.push(n);
 	while (!Q.empty()) {
-		int c = Q.front();
+		int now = Q.front();
+		cout << now << ' ';
 		Q.pop();
-		cout << c << ' ';
 		for (int i = 1; i <= N; i++) {
-			if (graph1[c][i] == 1 && !visit2[i]) {
-				visit2[i] = 1;
+			if (visit2[i] == 0 && graph[now][i]) {
+				visit2[i]++;
 				Q.push(i);
 			}
 		}
@@ -50,19 +34,20 @@ void bfs(int node) {
 }
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> N >> M >> V;
 	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
-		graph1[a][b] = 1;
-		graph1[b][a] = 1;
-
-		graph2[a].push_back(b);
-		graph2[b].push_back(a);
+		// a에서 b로, b에서 a로 가는 간선이 존재한다고 표시
+		graph[a][b] = graph[b][a] = 1;
 	}
-	for (int i = 0; i < graph2.size(); i++)
-		sort(graph2[i].begin(), graph2[i].end());
-	dfs(V);
+
+	visit1[V]++;
+	DFS(V);
 	cout << '\n';
-	bfs(V);
+	BFS(V);
+
 }
