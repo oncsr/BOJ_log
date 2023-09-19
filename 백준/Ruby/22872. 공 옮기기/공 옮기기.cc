205cfdf48a07f1ed4dc5b235bb2aebc8ea531831
oncsr <khj20006@naver.com>
2023-09-19 16:17:09 +0900

    [Ruby III] Title: 공 옮기기, Time: 0 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Ruby/22872. 공 옮기기/공 옮기기.cc
+++ 백준/Ruby/22872. 공 옮기기/공 옮기기.cc
@@ -2,122 +2,107 @@
 using namespace std;
 using ii = pair<int, int>;
 
-int n;
 vector<ii> ans;
 
-// 각 바구니의 공 개수
-int cnt[4]{};
+void one_two(int a, int b);
+void one_three(int a, int b);
+void two_one(int a, int b);
+void two_three(int a, int b);
+void three_one(int a, int b);
+void three_two(int a, int b);
 
-bool third_basket = 0;
-
-void solve(int a, int b, int s, int e) {
-	int o = 6 - s - e;
-	int ch1 = (cnt[s] - (b - a + 1)) & 1;
-	int ch2 = cnt[e] & 1;
+void one_two(int a, int b) {
 	if (a == b) {
-		ans.push_back({ s,e });
-		cnt[s]--, cnt[e]++;
+		ans.push_back({ 1,2 });
 		return;
 	}
 	if (a == b - 1) {
-		if (ch1 ^ ch2) {
-			ans.push_back({ s,e });
-			ans.push_back({ s,e });
-		}
-		else {
-			ans.push_back({ s,o });
-			ans.push_back({ s,e });
-			ans.push_back({ o,e });
-		}
-		cnt[s] -= 2, cnt[e] += 2;
+		ans.push_back({ 1,2 });
+		ans.push_back({ 1,2 });
 		return;
 	}
-	/*
-	if (a == b - 2) {
-		if (ch1 && ch2) {
-			ans.push_back({ s,e });
-			ans.push_back({ s,o });
-			ans.push_back({ e,o });
-			ans.push_back({ s,e });
-			ans.push_back({ o,s });
-			ans.push_back({ o,e });
-			ans.push_back({ s,e });
-		}
-		else if (ch2) {
-			ans.push_back({ s,o });
-			ans.push_back({ s,e });
-			ans.push_back({ s,e });
-			ans.push_back({ o,e });
-		}
-		else if (ch1) {
-			ans.push_back({ s,o });
-			ans.push_back({ s,e });
-			ans.push_back({ s,e });
-			ans.push_back({ o,e });
-		}
-		else {
-			ans.push_back({ s,o });
-			ans.push_back({ s,o });
-			ans.push_back({ s,e });
-			ans.push_back({ o,e });
-			ans.push_back({ o,e });
-		}
-		cnt[s] -= 3, cnt[e] += 3;
-		return;
-	}
-	*/
-	
-	/*
-	if (ch1 == 0 && ch2 == 0) {
-		solve(a + 1, b - 1, s, e);
-		ans.push_back({ s,o });
-		ans.push_back({ s,o });
-		cnt[s] -= 2, cnt[o] += 2;
-		solve(a + 1, b - 1, e, s);
-		ans.push_back({ o,e });
-		ans.push_back({ o,e });
-		cnt[o] -= 2, cnt[e] += 2;
-		solve(a + 1, b - 1, s, e);
-	}
-	else {
-		solve(a + 1, b - 1, s, o);
-		ans.push_back({ s,e });
-		ans.push_back({ s,e });
-		cnt[s] -= 2, cnt[e] += 2;
-		solve(a + 1, b - 1, o, e);
-	}
-	*/
-	solve(a + 1, b - 1, s, o);
-	ans.push_back({ s,e });
-	ans.push_back({ s,e });
-	cnt[s] -= 2, cnt[e] += 2;
-	solve(a + 1, b - 1, o, e);
+	one_three(a + 1, b - 1);
+	ans.push_back({ 1,2 });
+	ans.push_back({ 1,2 });
+	three_two(a + 1, b - 1);
 }
 
-void print() {
-	for (ii now : ans)
-		cout << now.first << ' ' << now.second << '\n';
+void one_three(int a, int b) {
+	if (a == b) {
+		ans.push_back({ 1,3 });
+		return;
+	}
+	if (a == b - 1) {
+		ans.push_back({ 1,3 });
+		ans.push_back({ 1,3 });
+		return;
+	}
+	one_two(a + 1, b - 1);
+	ans.push_back({ 1,3 });
+	ans.push_back({ 1,3 });
+	two_three(a + 1, b - 1);
+}
+
+void two_one(int a, int b) {
+	if (a == b) {
+		ans.push_back({ 2,1 });
+		return;
+	}
+	two_three(a + 1, b);
+	ans.push_back({ 2,1 });
+	three_one(a + 1, b);
+}
+
+void two_three(int a, int b) {
+	if (a == b) {
+		ans.push_back({ 2,3 });
+		return;
+	}
+	two_one(a, b - 1);
+	ans.push_back({ 2,3 });
+	one_three(a, b - 1);
+}
+
+void three_one(int a, int b) {
+	if (a == b) {
+		ans.push_back({ 3,1 });
+		return;
+	}
+	three_two(a, b - 1);
+	ans.push_back({ 3,1 });
+	two_one(a, b - 1);
+}
+
+void three_two(int a, int b) {
+	if (a == b) {
+		ans.push_back({ 3,2 });
+		return;
+	}
+	three_one(a + 1, b);
+	ans.push_back({ 3,2 });
+	one_two(a + 1, b);
 }
 
 int main() {
+	int n;
 	cin >> n;
-	cnt[1] = n;
 	if (n == 1) {
 		cout << "1\n1 3";
 		return 0;
 	}
-	if (n == 2) {
-		cout << "3\n1 2\n1 3\n2 3";
-		return 0;
+	one_two(2, n);
+	cout << ans.size() * 2 + 1 << '\n';
+	for (ii now : ans)
+		cout << now.first << ' ' << now.second << '\n';
+	cout << 1 << ' ' << 3 << '\n';
+	for (int i = ans.size() - 1; i >= 0; i--) {
+		int fir = ans[i].first;
+		int sec = ans[i].second;
+		if (sec == 1)	cout << 3 << ' ';
+		else if (sec == 3)	cout << 1 << ' ';
+		else	cout << sec << ' ';
+		if (fir == 1)	cout << 3 << '\n';
+		else if (fir == 3)	cout << 1 << '\n';
+		else	cout << fir << '\n';
 	}
-	solve(2, n, 1, 2);
-	ans.push_back({ 1,3 });
-	cnt[1]--, cnt[3]++;
-
-	solve(2, n, 2, 3);
-
-	cout << ans.size() << '\n';
-	
-	//print();
-
 }
