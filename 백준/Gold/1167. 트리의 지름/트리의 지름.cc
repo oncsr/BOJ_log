3b64178f48978401b1eb5e38888e139cd322724b
oncsr <khj20006@naver.com>
2023-10-12 14:50:46 +0900

    [Gold II] Title: 트리의 지름, Time: 72 ms, Memory: 8548 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,51 +1,40 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> ii;
-int N;
-vector<vector<ii> > V(100001);
-int cnt = 0;
-int idx = 0;
 
-int dfs(int node, int dist, int *visit) {
-	if (V[node].empty()) {
-		if (dist > cnt) {
-			cnt = dist;
-			idx = node;
+vector<vector<pair<int, int>>> V(100001);
+int mx = 0;
+
+int dfs(int n, int p) {
+	int mx1 = 0, mx2 = 0;
+	for (auto e : V[n]) {
+		int i = e.first, c = e.second;
+		if (i == p)	continue;
+		int g = dfs(i, n) + c;
+		if (g > mx1) {
+			mx2 = mx1;
+			mx1 = g;
 		}
+		else if (g > mx2)	mx2 = g;
 	}
-	else {
-		visit[node]++;
-		if (dist > cnt) {
-			cnt = dist;
-			idx = node;
-		}
-		for (int i = 0; i < V[node].size(); i++) {
-			if (!visit[V[node][i].first])
-				dfs(V[node][i].first, V[node][i].second + dist, visit);
-		}
-		return idx;
-	}
+	mx = max(mx, mx1 + mx2);
+	return mx1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N;
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
-		int n;
-		cin >> n;
-		int a, b;
+		int a;
 		cin >> a;
-		while (a != -1) {
-			cin >> b;
-			V[n].push_back({ a,b });
-			cin >> a;
+		int b, c;
+		for (cin >> b; b != -1; cin >> b) {
+			cin >> c;
+			V[a].push_back({ b,c });
 		}
 	}
-	int arr[100001] = { 0 };
-	int id = dfs(1, 0, arr);
-	int arr2[100001] = { 0 };
-	dfs(id, 0, arr2);
-	cout << cnt << '\n';
+	dfs(1, 0);
+	cout << mx;
+
 }
