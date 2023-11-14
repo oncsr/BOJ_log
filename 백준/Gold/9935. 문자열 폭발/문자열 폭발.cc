7add95a14bb82cb97354484b116fd9419dfc8dc3
oncsr <khj20006@naver.com>
2023-11-14 14:50:09 +0900

    [Gold IV] Title: 문자열 폭발, Time: 16 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,32 +1,50 @@
 #include <iostream>
-#include <map>
 #include <stack>
+#include <algorithm>
 using namespace std;
 
-/*
- < 2022.03.20 >
- [used algorithm]
- > string
- [point]
-
-*/
-
-int main() {
+int main(){
 	cin.tie(0)->sync_with_stdio(0);
 	string a, b;
-	cin >> a >> b;
-
+	cin>>a>>b;
+	stack<int> S;
 	string ans = "";
-	for (int i = 0; i < a.size(); i++) {
-		ans += a[i];
-		if (ans.size() < b.size())	continue;
-		string now = ans.substr(ans.size() - b.size(), b.size());
-		if (now == b) {
-			int g = b.size();
-			while (g--)	ans.pop_back();
+	int id = -1;
+	for(char i:a){
+		if(b[id+1] != i){
+			if(b[0] == i){
+				S.push(0);
+				id = 0;
+			}
+			else{
+				string temp = "";
+				while(!S.empty()){
+					temp += b[S.top()];
+					S.pop();
+				}
+				reverse(temp.begin(), temp.end());
+				ans += temp;
+				ans += i;
+				id = -1;
+			}
+			continue;
+		}
+		id++;
+		S.push(id);
+		if(id == b.size()-1){
+			for(int j=0;j<b.size();j++)
+				S.pop();
+			if(S.empty())	id = -1;
+			else 	id = S.top();
 		}
 	}
-	cout << (ans == "" ? "FRULA" : ans);
-
-
+	string temp = "";
+	while(!S.empty()){
+		temp += b[S.top()];
+		S.pop();
+	}
+	reverse(temp.begin(), temp.end());
+	ans += temp;
+	if(ans.empty())	cout<<"FRULA";
+	else 	cout<<ans;
 }
