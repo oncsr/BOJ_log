55c8bc1416b586ddc386e58206232dca0dff5151
oncsr <khj20006@naver.com>
2024-09-23 21:27:07 +0900

    [Diamond V] Title: 정육면체를 사랑하는 사람, Time: 132 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Diamond/14708. 정육면체를 사랑하는 사람/정육면체를 사랑하는 사람.cc
+++ 백준/Diamond/14708. 정육면체를 사랑하는 사람/정육면체를 사랑하는 사람.cc
@@ -7,12 +7,9 @@ int main() {
 
 	ll K;
 	cin >> K;
-	ll g = pow(K, 1. / 3);
-	ll mn = 9e18;
-	ll A = -1, B = -1, C = -1;
+	ll g = pow(K, 1. / 3), mn = 9e18, A = -1, B = -1, C = -1;
 	for (ll a = max(1LL, g - 2000); a <= g + 2000; a++)	for (ll b = max(1LL, g - 2000); b <= g + 2000; b++) {
-		ll c = (K - 1) / (a * b) + 1;
-		ll res = a * b + b * c + c * a;
+		ll c = (K - 1) / (a * b) + 1, res = a * b + b * c + c * a;
 		if (res < mn)	mn = res, A = a, B = b, C = c;
 	}
 	cout << A << ' ' << B << ' ' << C;
