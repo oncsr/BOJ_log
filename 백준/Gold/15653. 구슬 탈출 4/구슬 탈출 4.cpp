bc34aaf62b045df24bb204a8f19bc6ea81adf30f
oncsr <khj20006@naver.com>
2026-04-17 04:23:56 +0900

    [Gold 1] Title: 구슬 탈출 4, Time: 0 ms, Memory: 2024 KB

---
+++ 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int dx[4] = { 1,0,-1,0 };
+	int dy[4] = { 0,1,0,-1 };
+
+	int N, M;
+	cin >> N >> M;
+	char A[10][10]{};
+	int px, py, qx, qy;
+
+
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) {
+		cin >> A[i][j];
+		if (A[i][j] == 'R') px = i, py = j, A[i][j] = '.';
+		if (A[i][j] == 'B') qx = i, qy = j, A[i][j] = '.';
+	}
+
+	int vis[10][10][10][10]{};
+	vis[px][py][qx][qy]++;
+	queue<tuple<int, int, int, int, int>> Q;
+	Q.emplace(px, py, qx, qy, 0);
+	while (!Q.empty()) {
+		auto[ax, ay, bx, by, t] = Q.front();
+		Q.pop();
+		if (A[ax][ay] == 'O') return cout << t, 0;
+
+		for (int i = 0; i < 4; i++) {
+			int fx = ax, fy = ay, gx, gy;
+			while (!(fx == bx && fy == by) && A[fx][fy] == '.') fx += dx[i], fy += dy[i];
+			if (fx == bx && fy == by) {
+				gx = bx, gy = by;
+				while (A[gx][gy] == '.') gx += dx[i], gy += dy[i];
+				if (A[gx][gy] == 'O') continue;
+				gx -= dx[i], gy -= dy[i];
+				fx = gx - dx[i], fy = gy - dy[i];
+			}
+			else {
+				if (A[fx][fy] == '#') fx -= dx[i], fy -= dy[i];
+				gx = bx, gy = by;
+				while (!(gx == fx && gy == fy) && A[gx][gy] == '.') gx += dx[i], gy += dy[i];
+				if (A[gx][gy] == 'O') continue;
+				gx -= dx[i], gy -= dy[i];
+			}
+			if (vis[fx][fy][gx][gy]) continue;
+			vis[fx][fy][gx][gy]++;
+			Q.emplace(fx, fy, gx, gy, t + 1);
+		}
+	}
+	cout << -1;
+
+}
