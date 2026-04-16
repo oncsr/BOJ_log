2b1f4e7a601c194a2c92915ac7dcb1c046b33916
oncsr <khj20006@naver.com>
2026-04-17 03:39:00 +0900

    [Gold 5] Title: 줄어드는 수, Time: 124 ms, Memory: 14332 KB

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+class IOController {
+    BufferedReader br;
+    BufferedWriter bw;
+    StringTokenizer st;
+
+    public IOController() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer("");
+    }
+
+    String nextLine() throws Exception {
+        String line = br.readLine();
+        st = new StringTokenizer(line);
+        return line;
+    }
+
+    String nextToken() throws Exception {
+        while (!st.hasMoreTokens()) nextLine();
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws Exception {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws Exception {
+        return Double.parseDouble(nextToken());
+    }
+
+    void close() throws Exception {
+        bw.flush();
+        bw.close();
+    }
+
+    void write(String content) throws Exception {
+        bw.write(content);
+    }
+
+}
+
+public class Main {
+
+    static IOController io;
+
+    //
+
+    static int N;
+    static List<Long> list;
+
+    public static void main(String[] args) throws Exception {
+
+        io = new IOController();
+
+        init();
+        solve();
+
+        io.close();
+
+    }
+
+    public static void init() throws Exception {
+
+        N = io.nextInt();
+        list = new ArrayList<>();
+
+    }
+
+    static void solve() throws Exception {
+
+        for(int i=0;i<10;i++) bck(i);
+        Collections.sort(list);
+        if(N > list.size()) io.write("-1");
+        else io.write(list.get(N-1) + "\n");
+
+    }
+
+    static void bck(long cur) {
+        list.add(cur);
+        for(int i=0;i<cur%10;i++) bck(cur*10 + i);
+    }
+
+}
