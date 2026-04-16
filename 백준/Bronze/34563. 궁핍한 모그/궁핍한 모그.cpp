d8a3822c697a32cb89a8dfcdd553f3ced2ea989f
oncsr <khj20006@naver.com>
2026-04-17 02:17:03 +0900

    [Bronze 2] Title: 궁핍한 모그, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Bronze/34563. 궁핍한 모그/궁핍한 모그.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+
+    int T;
+    for(cin>>T;T--;){
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b-1<<'\n';
+    }
+}
