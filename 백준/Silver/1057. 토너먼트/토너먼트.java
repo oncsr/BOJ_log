c076f9d24c4c491eee55c47636d6c57597804b2d
oncsr <khj20006@naver.com>
2026-04-17 03:59:43 +0900

    [Silver 4] Title: 토너먼트, Time: 120 ms, Memory: 15880 KB

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+class IOController {
+    BufferedReader br;
+    BufferedWriter bw;
+    StringTokenizer st;
+
+    public IOController() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer("");
+    }
+
+    String nextLine() throws Exception {
+        String line = br.readLine();
+        st = new StringTokenizer(line);
+        return line;
+    }
+
+    String nextToken() throws Exception {
+        while (!st.hasMoreTokens()) nextLine();
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws Exception {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws Exception {
+        return Double.parseDouble(nextToken());
+    }
+
+    void close() throws Exception {
+        bw.flush();
+        bw.close();
+    }
+
+    void write(String content) throws Exception {
+        bw.write(content);
+    }
+
+}
+
+public class Main {
+
+    static IOController io;
+
+    //
+
+    static int N, A, B;
+
+    public static void main(String[] args) throws Exception {
+
+        io = new IOController();
+
+        init();
+        solve();
+
+        io.close();
+
+    }
+
+    public static void init() throws Exception {
+
+        N = io.nextInt();
+        A = io.nextInt();
+        B = io.nextInt();
+
+    }
+
+    static void solve() throws Exception {
+
+        int ans = 0;
+        while(A != B) {
+            A = (A+1)/2;
+            B = (B+1)/2;
+            ans++;
+        }
+        io.write(ans + "\n");
+
+    }
+
+}
