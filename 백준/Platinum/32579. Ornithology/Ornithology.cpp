4b88d64d93aea291decbeb94cde65a5b2de92431
oncsr <khj20006@naver.com>
2026-04-17 07:36:19 +0900

    [Platinum 5] Title: Ornithology, Time: 100 ms, Memory: 5736 KB

---
+++ 백준/Platinum/32579. Ornithology/Ornithology.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int seg[524289]{};
+
+void upt(int s, int e, int i, int n){
+	if(s == e)	{seg[n]++;return;}
+	int m=(s+e)>>1;
+	if(i <= m)	upt(s,m,i,n*2);
+	else 	upt(m+1,e,i,n*2+1);
+	seg[n] = seg[n*2] + seg[n*2+1];
+}
+
+int query(int s, int e, int l, int r, int n){
+	if(l>r||l>e||r<s)	return 0;
+	if(l<=s&&e<=r)	return seg[n];
+	int m=(s+e)>>1;
+	return query(s,m,l,r,n*2)+query(m+1,e,l,r,n*2+1);
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N;
+	cin>>N;
+	ll ans = 0;
+	for(int i=1,p,a;i<=N;i++){
+		vector<int> P;
+		for(cin>>p;p--;){
+			cin>>a;
+			ans += query(1,N,++a + 1,N,1);
+			P.push_back(a);
+		}
+		sort(P.begin(),P.end());
+		for(int &j:P)	upt(1,N,j,1);
+	}
+	cout<<ans;
+
+}
