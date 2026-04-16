6d953e4ed8a75b1cb9eb4d8ea5caba6b22ccfb34
oncsr <khj20006@naver.com>
2026-04-17 04:11:50 +0900

    [Bronze 3] Title: 쌍의 합, Time: 120 ms, Memory: 16076 KB

---
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		
+		for(int T=nextInt();T-->0;solve()) ready();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		bw.write("Pairs for " + N + ": ");
+		for(int a=1;a<N-a;a++) {
+			if(a != 1) bw.write(", ");
+			bw.write(a + " " + (N-a));
+		}
+		bw.write("\n");
+		
+	}
+	
+}
