6254c61d19e5d01f3cd01f775dd25025b2bae5db
oncsr <khj20006@naver.com>
2026-04-17 03:35:22 +0900

    [Gold 4] Title: 동전 1, Time: 136 ms, Memory: 15800 KB

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+class IOController {
+    BufferedReader br;
+    BufferedWriter bw;
+    StringTokenizer st;
+
+    public IOController() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer("");
+    }
+
+    String nextLine() throws Exception {
+        String line = br.readLine();
+        st = new StringTokenizer(line);
+        return line;
+    }
+
+    String nextToken() throws Exception {
+        while (!st.hasMoreTokens()) nextLine();
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws Exception {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws Exception {
+        return Double.parseDouble(nextToken());
+    }
+
+    void close() throws Exception {
+        bw.flush();
+        bw.close();
+    }
+
+    void write(String content) throws Exception {
+        bw.write(content);
+    }
+
+}
+
+public class Main {
+
+    static IOController io;
+
+    //
+
+    static int N, K;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+
+        io = new IOController();
+
+        init();
+        solve();
+
+        io.close();
+
+    }
+
+    static void init() throws Exception {
+
+        N = io.nextInt();
+        K = io.nextInt();
+        dp = new int[K+1];
+
+    }
+
+    static void solve() throws Exception {
+
+        dp[0] = 1;
+        while(N-->0) {
+            int a = io.nextInt();
+            for(int k=0;k+a<=K;k++) if(dp[k] != 0) dp[k+a] += dp[k];
+        }
+        io.write(dp[K] + "\n");
+
+    }
+
+}
