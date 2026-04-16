e154920315bc0ddbf55de11f837d6e18db58b711
oncsr <khj20006@naver.com>
2026-04-17 08:23:00 +0900

    [Bronze 4] Title: Simple Sum, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int a, b, c;
+	char _;
+	cin >> a >> _ >> b >> _ >> c;
+	cout << (a + b == c ? "YES" : "NO");
+
+}
