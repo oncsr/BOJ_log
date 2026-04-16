72732bdfa2e56d12eacb7b8a80aad0d0959a9d7c
oncsr <khj20006@naver.com>
2026-04-17 04:17:14 +0900

    [Gold 3] Title: 군사 이동, Time: 400 ms, Memory: 33752 KB

---
+++ 백준/Gold/11085. 군사 이동/군사 이동.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N, M, S, E;
+	static int[][] G;
+	static int[] r;
+	
+	static int f(int x) {return x==r[x] ? x : (r[x]=f(r[x]));}
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		M = nextInt();
+		S = nextInt();
+		E = nextInt();
+		r = new int[N];
+		for(int i=0;i<N;i++) r[i] = i;
+		G = new int[M][];
+		for(int i=0;i<M;i++) G[i] = new int[] {nextInt(), nextInt(), nextInt()};
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		Arrays.sort(G, (a,b) -> b[2]-a[2]);
+		for(int[] e:G) {
+			int a = e[0], b = e[1], c = e[2];
+			int x = f(a), y = f(b);
+			if(x == y) continue;
+			r[x] = y;
+			if(f(S) == f(E)) {
+				bw.write(c + "\n");
+				return;
+			}
+		}
+		
+	}
+	
+}
