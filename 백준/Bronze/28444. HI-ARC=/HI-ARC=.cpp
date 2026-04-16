ddadcf1e4b00796ba3d730efc2b1b9e5c053636c
oncsr <khj20006@naver.com>
2026-04-17 06:58:58 +0900

    [Bronze 5] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Bronze/28444. HI-ARC=/HI-ARC=.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int h, i, a, r, c;
+	cin >> h >> i >> a >> r >> c;
+	cout << h * i - a * r * c;
+	
+}
