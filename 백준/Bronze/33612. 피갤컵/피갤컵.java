e947e78435e45b997113dbd161cb6c4d648600cc
oncsr <khj20006@naver.com>
2026-04-17 05:20:43 +0900

    [Bronze 5] Title: 피갤컵, Time: 124 ms, Memory: 15940 KB

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static int nextInt() throws Exception {
+		if(!st.hasMoreTokens()) nextLine();
+		return Integer.parseInt(st.nextToken());
+	}
+	static long nextLong() throws Exception {
+		if(!st.hasMoreTokens()) nextLine();
+		return Long.parseLong(st.nextToken());
+	}
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N;
+	static int year = 2024, month = 1;
+	
+	public static void main(String[] args) throws Exception {
+			
+		ready();
+		solve();
+	
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		
+	}
+	
+	static void solve() throws Exception{
+		
+		for(int i=0;i<N;i++) {
+			month += 7;
+			if(month > 12) {
+				month -= 12;
+				year++;
+			}
+		}
+		
+		bw.write(year + " " + month);
+	}
+	
+}
