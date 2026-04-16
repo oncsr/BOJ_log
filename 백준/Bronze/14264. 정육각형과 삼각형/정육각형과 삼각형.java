71eeef4bc623efb181931a24bf10fabafcee1800
oncsr <khj20006@naver.com>
2026-04-17 05:16:18 +0900

    [Bronze 3] Title: 정육각형과 삼각형, Time: 120 ms, Memory: 16156 KB

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		if(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static double L;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+	
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		L = nextDouble();
+		
+	}
+	
+	static void solve() throws Exception{
+		
+		bw.write((L*L*0.4330127018922193233818) + "\n");
+
+	}
+	
+}
