48e0ae66ddf543ddfef6a2c52a2238f8b441be10
oncsr <khj20006@naver.com>
2026-04-17 05:48:48 +0900

    [Bronze 5] Title: 노트북 세 대를 가지고 왔다, Time: 120 ms, Memory: 15868 KB

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static int nextInt() {return Integer.parseInt(st.nextToken());}
+	static long nextLong() {return Long.parseLong(st.nextToken());}
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int T1, T2;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+	
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+
+		nextLine();
+		T1 = nextInt();
+		T2 = nextInt();
+		
+	}
+	
+	static void solve() throws Exception{
+
+		bw.write(Math.min(T1, T2) + "\n");
+
+	}
+	
+}
