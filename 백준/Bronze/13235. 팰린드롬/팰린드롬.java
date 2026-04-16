d82d9dd9c1693476d040451a1a9ddf98d615d4c6
oncsr <khj20006@naver.com>
2026-04-17 04:31:45 +0900

    [Bronze 2] Title: 팰린드롬, Time: 104 ms, Memory: 14084 KB

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static char[] S;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		S = br.readLine().toCharArray();
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		for(int i=0,j=S.length-1;i<j;i++,j--) {
+			if(S[i] != S[j]) {
+				bw.write("false");
+				return;
+			}
+		}
+		bw.write("true");
+		
+	}
+	
+}
