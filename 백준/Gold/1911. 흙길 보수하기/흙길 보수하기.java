b044d25b7f619db2c947caea41a99e11ab139359
oncsr <khj20006@naver.com>
2026-04-17 04:34:01 +0900

    [Gold 5] Title: 흙길 보수하기, Time: 244 ms, Memory: 20660 KB

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -1,86 +1,60 @@
-import java.io.*;
 import java.util.*;
-
-class IOController {
-	BufferedReader br;
-	BufferedWriter bw;
-	StringTokenizer st;
-
-	public IOController() {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		st = new StringTokenizer("");
-	}
-
-	String nextLine() throws Exception {
-		String line = br.readLine();
-		st = new StringTokenizer(line);
-		return line;
-	}
-
-	String nextToken() throws Exception {
-		while (!st.hasMoreTokens())
-			nextLine();
-		return st.nextToken();
-	}
-
-	int nextInt() throws Exception {
-		return Integer.parseInt(nextToken());
-	}
-
-	long nextLong() throws Exception {
-		return Long.parseLong(nextToken());
-	}
-
-	double nextDouble() throws Exception {
-		return Double.parseDouble(nextToken());
-	}
-
-	void close() throws Exception {
-		bw.flush();
-		bw.close();
-	}
-
-	void write(String content) throws Exception {
-		bw.write(content);
-	}
-
-}
+import java.io.*;
 
 public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
 
-	static IOController io;
-
-	//
-
-	static int N, L;
-	static int[][] arr;
-
-	public static void main(String[] args) throws Exception {
-
-		io = new IOController();
-
-		N = io.nextInt();
-		L = io.nextInt();
-		arr = new int[N][];
-		for(int i=0;i<N;i++) arr[i] = new int[]{io.nextInt(),io.nextInt()};
-
-		Arrays.sort(arr, (a,b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
-
-		int cnt = 0, prev = -1;
-		for(int[] oasis : arr) {
-			int s = oasis[0], e = oasis[1];
-			s = Math.max(s, prev);
-			if(s >= e) continue;
-			int len = e-s;
-			int need = (len-1)/L + 1;
-			cnt += need;
-			prev = s + need*L;
-		}
-		io.write(cnt + "\n");
-
-		io.close();
-
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
 	}
-
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N, L;
+	static int[][] A;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		L = nextInt();
+		A = new int[N][2];
+		for(int i=0;i<N;i++) for(int j=0;j<2;j++) A[i][j] = nextInt();
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		Arrays.sort(A, (a,b) -> a[0]-b[0]);
+		int ans = 0, next = -1;
+		for(int i=0;i<N;i++) {
+			int s = A[i][0], e = A[i][1];
+			if(next >= e) continue;
+			next = Math.max(s, next);
+			int temp = (e-next-1)/L + 1;
+			ans += temp;
+			next += temp*L;
+		}
+		bw.write(ans + "\n");
+		
+	}
+	
 }
