58abfed551466f9b4454a8a913dfe44c31ef80aa
oncsr <khj20006@naver.com>
2026-04-17 03:52:59 +0900

    [Silver 2] Title: 수 이어 쓰기, Time: 152 ms, Memory: 16920 KB

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+class IOController {
+    BufferedReader br;
+    BufferedWriter bw;
+    StringTokenizer st;
+
+    public IOController() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer("");
+    }
+
+    String nextLine() throws Exception {
+        String line = br.readLine();
+        st = new StringTokenizer(line);
+        return line;
+    }
+
+    String nextToken() throws Exception {
+        while (!st.hasMoreTokens()) nextLine();
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws Exception {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws Exception {
+        return Double.parseDouble(nextToken());
+    }
+
+    void close() throws Exception {
+        bw.flush();
+        bw.close();
+    }
+
+    void write(String content) throws Exception {
+        bw.write(content);
+    }
+
+}
+
+public class Main {
+
+    static IOController io;
+
+    /////////////////////////////////
+
+    static char[] A;
+
+    public static void main(String[] args) throws Exception {
+
+        io = new IOController();
+
+        init();
+        solve();
+
+        io.close();
+
+    }
+
+    public static void init() throws Exception {
+
+        A = io.nextLine().toCharArray();
+
+    }
+
+    static void solve() throws Exception {
+
+        int ans = 0, idx = 0;
+        while(idx < A.length) {
+            ans++;
+            for(char i : Integer.toString(ans).toCharArray()){
+                if(A[idx] == i) idx++;
+                if(idx == A.length) break;
+            }
+        }
+        io.write(ans + "\n");
+
+    }
+
+    /////////////////////////////////
+
+}
