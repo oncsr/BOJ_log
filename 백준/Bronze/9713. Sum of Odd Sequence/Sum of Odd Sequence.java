b3e7ced07ded66e794045370d9cbd1f9d7fd2a1d
oncsr <khj20006@naver.com>
2026-04-17 04:11:24 +0900

    [Bronze 3] Title: Sum of Odd Sequence, Time: 116 ms, Memory: 15772 KB

---
+++ 백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		
+		for(int T=nextInt();T-->0;solve()) ready();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		int ans = 0;
+		for(int i=1;i<=N;i+=2) ans += i;
+		bw.write(ans + "\n");
+		
+	}
+	
+}
