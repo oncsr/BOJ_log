9ceb23feee01ac208f5d613f4c16a6bafebb7d7c
oncsr <khj20006@naver.com>
2026-04-17 07:32:05 +0900

    [Silver 4] Title: Niekolejne, Time: 0 ms, Memory: 2032 KB

---
+++ 백준/Silver/8261. Niekolejne/Niekolejne.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pl = pair<ll, ll>;
+
+void sol() {
+
+	int TC = 1;
+	//cin >> TC;
+	for (; TC--;) {
+
+		ll N;
+		cin >> N;
+		cout << ll(sqrtl(N));
+
+	}
+
+}
+
+int main() {
+
+	sol();
+
+}
