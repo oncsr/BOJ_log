a3f08e34b283c15210766bcb16b3763682bab10e
oncsr <khj20006@naver.com>
2026-04-17 04:11:05 +0900

    [Bronze 4] Title: Which Alien?, Time: 100 ms, Memory: 14228 KB

---
+++ 백준/Bronze/6778. Which Alien/Which Alien.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int a, b;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+		
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		a = nextInt();
+		b = nextInt();
+		
+	}
+	
+	static void solve() throws Exception {
+		
+		if(a>=3 && b<=4) bw.write("TroyMartian\n");
+		if(a<=6 && b>=2) bw.write("VladSaturnian\n");
+		if(a<=2 && b<=3) bw.write("GraemeMercurian\n");
+		
+	}
+	
+}
