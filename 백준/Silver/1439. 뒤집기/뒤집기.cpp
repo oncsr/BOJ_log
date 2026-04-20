0080dd11433c2b58bc55501f562303fceb6e0401
oncsr <khj20006@naver.com>
2026-04-21 08:58:44 +0900

    [Silver 5] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB

--- 백준/Silver/1439. 뒤집기/뒤집기.cpp
+++ 백준/Silver/1439. 뒤집기/뒤집기.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 
-	char now, prev = ' ';
-	int one = 0, zero = 0;
-	while (cin >> now) {
-		if (now != prev) {
-			if (now == '0')	zero++;
-			else	one++;
-		}
-		prev = now;
+	string s;
+	cin >> s;
+
+	int cnt[2]{};
+	char temp = s[0];
+	for (char i : s) {
+		if (temp != i)	cnt[temp - '0']++;
+		temp = i;
 	}
-	cout << min(one, zero);
+	cnt[temp - '0']++;
+
+	cout << min(cnt[0], cnt[1]);
 
 }
