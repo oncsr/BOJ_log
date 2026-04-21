0abba3659bc1ec954c3e4dfa157c7576442212fb
oncsr <khj20006@naver.com>
2026-04-21 10:01:00 +0900

    [Platinum 4] Title: 멀티탭 스케줄링 2, Time: 280 ms, Memory: 44580 KB

--- 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.cpp
+++ 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.cpp
@@ -4,31 +4,44 @@ using ll = long long;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
+	
+	int arr[500001]{};
+	vector<vector<int>> EX(500001);
+	vector<int> pos(500001);
 	int N, K;
 	cin >> N >> K;
-	queue<int> next[500001]{};
-	int d[500001]{};
-	int arr[500001]{};
 	for (int i = 1; i <= K; i++) {
 		cin >> arr[i];
-		next[arr[i]].emplace(i);
+		EX[arr[i]].push_back(i);
 	}
-	for (int i = 1; i <= K; i++)	next[i].emplace(K + 1);
+	for (int i = 1; i <= K; i++)	EX[i].push_back(K + 1);
 
-	int use[500001]{}, cnt = 0, ans = 0;
+	int use[500001]{}, cnt = 0;
+	int ans = 0;
 	priority_queue<pair<int, int>> Q;
 	for (int i = 1; i <= K; i++) {
-		int a = arr[i];
-		if (use[a]) { next[a].pop(); Q.emplace(next[a].front(), a); continue; }
-		if (cnt < N) { use[a] ^= 1; next[a].pop(); Q.emplace(next[a].front(), a); cnt++; continue; }
+		int to = arr[i];
+		if (use[to]) {
+			pos[to]++;
+			Q.push({ EX[to][pos[to]], to });
+			continue;
+		}
+
+		if (cnt < N) {
+			pos[to]++;
+			use[to] = 1;
+			cnt++;
+			Q.push({ EX[to][pos[to]], to });
+			continue;
+		}
+
+		int fr = Q.top().second, wit = Q.top().first;
+		Q.pop();
+		use[to] = 1;
+		use[fr] = 0;
+		pos[to]++;
+		Q.push({ EX[to][pos[to]], to });
 		ans++;
-		int rem = Q.top().second;
-		Q.pop(); 
-		use[rem] ^= 1;
-		use[a] ^= 1;
-		next[a].pop();
-		Q.emplace(next[a].front(), a);
 	}
 	cout << ans;
 
