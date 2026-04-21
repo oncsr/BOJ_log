bdc6be1936062b116ee10ffac8625d180f5d208d
oncsr <khj20006@naver.com>
2026-04-21 10:26:41 +0900

    [Silver 3] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB

--- 백준/Silver/15655. N과 M (6)/N과 M (6).cpp
+++ 백준/Silver/15655. N과 M (6)/N과 M (6).cpp
@@ -4,31 +4,31 @@ using namespace std;
 int N, M;
 int arr[8]{};
 
-void bck(int cur, vector<int> select) {
-	if (select.size() == M) {
-		for (int i : select) cout << i << ' ';
-		cout << '\n';
+void Backtracking(int now, vector<int> select) {
+
+	if (now == N) {
+		if (select.size() == M) {
+			for (int i : select)	cout << i << ' ';
+			cout << '\n';
+		}
 		return;
 	}
 
-	if (select.size() > M) return;
-	if (cur == N) return;
+	select.push_back(arr[now]);
+	Backtracking(now + 1, select);
 
-	// cur을 고른 경우
-	select.push_back(arr[cur]);
-	bck(cur + 1, select);
-	// cur을 고르지 않은 경우
 	select.pop_back();
-	bck(cur + 1, select);
+	Backtracking(now + 1, select);
+
 }
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
 
+int main() {
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) cin >> arr[i];
+	for (int i = 0; i < N; i++)	cin >> arr[i];
+
 	sort(arr, arr + N);
 
-	bck(0, {});
+	Backtracking(0, {});
 
 }
