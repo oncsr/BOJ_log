0f9d258b04bd38da709b94443f6b2d330fcc6ab5
oncsr <khj20006@naver.com>
2026-04-21 09:54:47 +0900

    [Bronze 4] Title: 감마선을 맞은 컴퓨터, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+
+    char a;
+    while(cin>>a){
+        if(a == 'w')    {cout<<"chunbae";return 0;}
+        if(a == 'b')    {cout<<"nabi";return 0;}
+        if(a == 'g')    {cout<<"yeongcheol";return 0;}
+    }
+}
