6eacc7498cb245f8c08d636210817b68224fadf9
oncsr <khj20006@naver.com>
2026-04-23 07:50:15 +0900

    [Gold 1] Title: 제곱 ㄴㄴ 수, Time: 24 ms, Memory: 17524 KB

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cpp
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cpp
@@ -1,29 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 using ll = long long;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
-	ll A, B;
-	cin >> A >> B;
-
-	int e[1000001]{};
-	for (int i = 2; i * i <= 1000000; i++)	if (!e[i])	for (int j = i * i; j <= 1000000; j += i)	e[j] = 1;
-
-	int ans = B - A + 1;
-	int visit[1000001]{};
-	for (ll i = 2; i <= 1000000; i++) {
-		if (!e[i]) {
-			ll k;
-			if (A % (i * i) == 0)	k = A / (i * i) * (i * i);
-			else	k = (A / (i * i) + 1) * (i * i);
-			while (k <= B) {
-				if (!visit[k - A])	ans--, visit[k - A]++;
-				k += i * i;
-			}
+	ll e[1000001]{};
+	ll arr[1000001]{};
+	ll mn, mx, s;
+	cin >> mn >> mx;
+	s = mx - mn + 1;
+	for (ll i = 2; i * i <= mx; i++) {
+		if (e[i])	continue;
+		for (ll j = i * 2; j <= 1000000; j += i)	e[j] = 1;
+		ll k = i * i;
+		for (ll a = (mn / k * k == mn ? mn : mn / k * k + k); a <= mx; a += k) {
+			if (!arr[a - mn])	s--;
+			arr[a - mn]++;
 		}
 	}
-	cout << ans;
+	cout << s;
 
 }
