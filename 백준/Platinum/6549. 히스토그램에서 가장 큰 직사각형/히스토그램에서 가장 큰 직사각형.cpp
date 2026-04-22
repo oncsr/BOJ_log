819093a811a20e5d99d9b0090b6271a0f1ff5f5d
oncsr <khj20006@naver.com>
2026-04-23 07:00:11 +0900

    [Platinum 5] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 3744 KB

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+using ll = long long;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N;
+	for (cin >> N; N; cin >> N) {
+		stack<pair<ll, int> > S;
+		ll a, mx = 0;
+		for (int i = 0; i < N; i++) {
+			cin >> a;
+			if (S.empty())	S.push({ a,i });
+			else {
+				mx = max(mx, a);
+				while (!S.empty() && S.top().first > a) {
+					ll now = S.top().second;
+					ll height = S.top().first;
+					S.pop();
+					while (!S.empty() && S.top().first == height) {
+						S.pop();
+					}
+					if (S.empty()) {
+						mx = max(mx, i * height);
+					}
+					else {
+						mx = max(mx, (((ll)i - 1) - S.top().second) * height);
+					}
+				}
+				S.push({ a,i });
+			}
+		}
+		while (!S.empty()) {
+			ll now = S.top().second;
+			ll height = S.top().first;
+			S.pop();
+			while (!S.empty() && S.top().first == height) {
+				S.pop();
+			}
+			if (S.empty()) {
+				mx = max(mx, N * height);
+			}
+			else {
+				mx = max(mx, ((ll)N - ((ll)S.top().second + 1)) * height);
+			}
+		}
+		cout << mx << '\n';
+
+	}
+}
