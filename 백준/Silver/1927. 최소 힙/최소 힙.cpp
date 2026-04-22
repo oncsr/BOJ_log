08622711a776cfd9639d7095d34fc5e9816e0edc
oncsr <khj20006@naver.com>
2026-04-23 07:04:20 +0900

    [Silver 2] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, a;
+	priority_queue<int, vector<int>, greater<>>Q;
+	for (cin >> N; N--;) {
+		cin >> a;
+		if (a)	Q.push(a);
+		else if (Q.empty())	cout << "0\n";
+		else	cout << Q.top() << '\n', Q.pop();
+	}
+}
