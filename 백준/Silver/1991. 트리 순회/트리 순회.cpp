c64b0edc3f7aa19e61a39e52072a23fc82ecb3e7
oncsr <khj20006@naver.com>
2026-04-23 07:34:26 +0900

    [Silver 1] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB

--- 백준/Silver/1991. 트리 순회/트리 순회.cpp
+++ 백준/Silver/1991. 트리 순회/트리 순회.cpp
@@ -1,26 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define left first
+#define right second
 
-vector<char> L(126, -1), R(126, -1);
-string pre = "", in = "", post = "";
+pair<char, char> Tree[27];
+int N;
 
-void dfs(char a) {
-	pre += a;
-	if (L[a] != '.')	dfs(L[a]);
-	in += a;
-	if (R[a] != '.')	dfs(R[a]);
-	post += a;
+void preorder(char node) {
+	if (node == '.')	return;
+	cout << node;
+	int temp = node - 'A';
+	preorder(Tree[temp].left);
+	preorder(Tree[temp].right);
+}
+
+void inorder(char node) {
+	if (node == '.')	return;
+	int temp = node - 'A';
+	inorder(Tree[temp].left);
+	cout << node;
+	inorder(Tree[temp].right);
+}
+
+void postorder(char node) {
+	if (node == '.')	return;
+	int temp = node - 'A';
+	postorder(Tree[temp].left);
+	postorder(Tree[temp].right);
+	cout << node;
+
 }
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	int N;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		char A, B, C;
-		cin >> A >> B >> C;
-		L[A] = B, R[A] = C;
+	for (int i = 0; i < N; i++) {
+		char a, b, c;
+		cin >> a >> b >> c;
+		int x = a - 'A';
+		Tree[x] = { b,c };
 	}
-	dfs('A');
-	cout << pre << '\n' << in << '\n' << post;
+
+	preorder('A');
+	cout << '\n';
+
+	inorder('A');
+	cout << '\n';
+
+	postorder('A');
+	cout << '\n';
+
 }
