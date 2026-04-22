6c9c8caddb68a051f0ede98b6bb4dd74cccd67dc
oncsr <khj20006@naver.com>
2026-04-23 06:27:10 +0900

    [Gold 4] Title: 두 로봇, Time: 60 ms, Memory: 17640 KB

--- 백준/Gold/15971. 두 로봇/두 로봇.cpp
+++ 백준/Gold/15971. 두 로봇/두 로봇.cpp
@@ -1,40 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<pair<int, int>>> V(100001);
-tuple<int, int, int> par[100001]{};
+vector<vector<pair<int, int> > > V(100001);
+int s = 0, mn = 0;
+int N, S, E;
+vector<int> way;
 
-void dfs(int n, int p, int s) {
-	for (auto [i, c] : V[n]) {
-		if (i == p)	continue;
-		par[i] = { n, c, s + c };
-		dfs(i, n, s + c);
+bool dfs(int n, int p, int d) {
+	if (n == E) {
+		mn = s = d;
+		return true;
+	}
+	for (pair<int, int> i : V[n]) {
+		int next = i.first, cost = i.second;
+		bool f = 0;
+		if (next != p) {
+			f |= dfs(next, n, d + cost);
+			if (f) {
+				way.push_back(next);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void dfs2(int n, int p, int id, int tar) {
+	for (pair<int, int> i : V[n]) {
+		int next = i.first, cost = i.second;
+		if (next == tar) {
+			mn = min(mn, s - cost);
+			if (id < way.size() - 1)	dfs2(next, n, id + 1, way[id + 1]);
+			else	return;
+		}
 	}
 }
 
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	
-	int N, A, B;
-	cin >> N >> A >> B;
+	cin >> N >> S >> E;
 	for (int i = 1; i < N; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		V[a].emplace_back(b, c);
-		V[b].emplace_back(a, c);
+		int u, v, d;
+		cin >> u >> v >> d;
+		V[u].push_back({ v,d });
+		V[v].push_back({ u,d });
 	}
-
-	dfs(A, 0, 0);
-
-	if (A == B)	return cout << 0, 0;
-	int ans = 2e9, s = 0;
-	while (B != A) {
-		auto [p, c, t] = par[B];
-		ans = min(ans, s + t - c);
-		s += c;
-		B = p;
-	}
-	cout << ans;
-
-
+	dfs(S, 0, 0);
+	way.push_back(S);
+	int id = 0;
+	dfs2(way[id], 0, id + 1, way[id + 1]);
+	cout << mn;
 }
