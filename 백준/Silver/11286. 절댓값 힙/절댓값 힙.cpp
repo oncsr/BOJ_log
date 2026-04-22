4b010ab2ee208aaf6cb70d929298b246fa7773fc
oncsr <khj20006@naver.com>
2026-04-23 07:03:38 +0900

    [Silver 1] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cpp
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cpp
@@ -1,2 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;using I=pair<unsigned,int>;main(){int N,x;priority_queue<I,vector<I>,greater<>>Q;for(cin>>N;N--;){cin>>x;x?Q.push({abs(x),x}):(Q.push({3e9,0}),cout<<Q.top().second<<'\n',Q.pop());}}
+#include <iostream>
+#include <queue>
+using namespace std;
+struct cmp {
+	bool operator()(int a, int b) {
+		return abs(a) == abs(b) ? a > b:abs(a) > abs(b);
+	}
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, a;
+	priority_queue<int, vector<int>, cmp>Q;
+	for (cin >> N; N--;) {
+		cin >> a;
+		if (a)	Q.push(a);
+		else if (Q.empty())	cout << "0\n";
+		else	cout << Q.top() << '\n', Q.pop();
+	}
+}
