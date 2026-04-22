d9e30b028839b2b2a899af5177ab7c3aaf2c2c8d
oncsr <khj20006@naver.com>
2026-04-23 06:55:16 +0900

    [Silver 4] Title: 문자열 집합, Time: 312 ms, Memory: 9684 KB

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cpp
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cpp
@@ -1,27 +1,3 @@
-#include <iostream>
-#include <set>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-
-	int N, M;
-	cin >> N >> M;
-
-	set<string> A;
-	for (int i = 0; i < N; i++) {
-		string a;
-		cin >> a;
-		A.insert(a);
-	}
-
-	int answer = 0;
-	for (int i = 0; i < M; i++) {
-		string a;
-		cin >> a;
-		if (A.count(a))	answer++;
-	}
-
-	cout << answer;
-
-}
+main(){map<string,int>S;int N,M,c=0;string a;for(cin>>N>>M;N--;)cin>>a,S[a]=1;for(;M--;)cin>>a,c+=S[a];cout<<c;}
