85e864c45873a660cd3845dc2dd320a9a29616da
oncsr <khj20006@naver.com>
2026-04-23 08:19:16 +0900

    [Gold 5] Title: 튕기는 볼링공, Time: 0 ms, Memory: 2148 KB

---
+++ 백준/Gold/1876. 튕기는 볼링공/튕기는 볼링공.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N;
+    double PI = 3.141592653589793;
+	for (cin >> N; N--;) {
+		double T, X;
+		cin >> T >> X;
+		T *= 100.0;
+		double t = tan(X * PI / 180.0);
+		int cross = 0;
+		double Q = 0;
+		if (-T * t < -42.5) {
+			Q = -42.5;
+			while (-T * t < Q) {
+				cross++;
+				Q -= 85.0;
+			}
+		}
+		Q = -85.0 * cross;
+		if (abs(T * t + Q) / sqrt(t * t + 1.0) < 16.0)	cout << "yes\n";
+		else	cout << "no\n";
+	}
+}
