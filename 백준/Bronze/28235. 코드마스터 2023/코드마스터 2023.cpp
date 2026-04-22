9b5623b9be8435de675f19bf8a8aeaa2f273829a
oncsr <khj20006@naver.com>
2026-04-23 06:26:46 +0900

    [Bronze 5] Title: 코드마스터 2023, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a;
+	cin >> a;
+	if (a == 'S')	cout << "HIGHSCHOOL";
+	if (a == 'C')	cout << "MASTER";
+	if (a == '2')	cout << "0611";
+	if (a == 'A')	cout << "CONTEST";
+}
