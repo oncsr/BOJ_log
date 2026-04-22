b1f3e1b603398c6abf7a82effe8b83965dbae930
oncsr <khj20006@naver.com>
2026-04-23 07:36:33 +0900

    [Gold 4] Title: 좋다, Time: 120 ms, Memory: 2020 KB

--- 백준/Gold/1253. 좋다/좋다.cpp
+++ 백준/Gold/1253. 좋다/좋다.cpp
@@ -1,38 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
-	int N;
-	int arr[2001]{};
+	int N, arr[2000]{}, vis[2000]{};
 	cin >> N;
 	for (int i = 0; i < N; i++)	cin >> arr[i];
-
 	sort(arr, arr + N);
 
-	int ans = 0;
-	for (int k = 0; k < N; k++) {
-		int s = 0, e = N - 1;
-		while (s < e) {
-			if (arr[s] + arr[e] == arr[k]) {
-				if (s != k && e != k) {
-					ans++;
-					break;
-				}
-				else {
-					if (s == k)	s++;
-					else if (e == k)	e--;
-				}
-			}
-			else if (arr[s] + arr[e] > arr[k]) {
-				e--;
-			}
-			else {
-				s++;
-			}
+	int s = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			int ch = arr[i] + arr[j];
+			int upp = upper_bound(arr, arr + N, ch) - arr;
+			int low = lower_bound(arr, arr + N, ch) - arr;
+			if (upp == low)	continue;
+			if (vis[low])	continue;
+			if (low == N || low == i || low == j)	continue;
+			int temp = arr[low];
+			for (int k = low; k < upp; k++)	vis[k]++, s++;
 		}
 	}
-	cout << ans;
-
+	cout << s;
 }
