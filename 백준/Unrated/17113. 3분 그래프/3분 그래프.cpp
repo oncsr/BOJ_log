18f6d12110d7c152db8ef4a773392064840efe88
oncsr <khj20006@naver.com>
2026-04-23 08:47:57 +0900

    [Unrated] Title: 3분 그래프, Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Unrated/17113. 3분 그래프/3분 그래프.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n=1;
+    // nnnnnnnn
+    // n
+    // 1 2 3 4 5 6 7 8 9 10 11 12 13
+    // sqqwwwwwsqeeqwws
+    // I'm happy
+    
+    // 3qns djswp wlskrk
+    // nnnnnnnn
+    // 1111111111
+    // 3.14159265358979323
+    // e^(iㅠ) = -1
+    // ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ
+    
+}
