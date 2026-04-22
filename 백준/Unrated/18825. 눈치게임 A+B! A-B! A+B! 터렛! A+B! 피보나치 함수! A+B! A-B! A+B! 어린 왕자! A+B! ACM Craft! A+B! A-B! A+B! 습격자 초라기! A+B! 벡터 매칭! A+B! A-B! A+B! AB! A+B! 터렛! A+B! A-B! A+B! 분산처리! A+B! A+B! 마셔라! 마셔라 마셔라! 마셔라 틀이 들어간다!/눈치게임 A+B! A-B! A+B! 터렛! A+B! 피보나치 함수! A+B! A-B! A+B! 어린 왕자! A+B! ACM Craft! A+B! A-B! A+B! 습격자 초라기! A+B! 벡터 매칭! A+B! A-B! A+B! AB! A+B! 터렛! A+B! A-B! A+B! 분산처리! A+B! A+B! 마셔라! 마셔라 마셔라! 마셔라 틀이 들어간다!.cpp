04c5e2c077e8a8c4c5f2637a4c3f1d2fc9c31db8
oncsr <khj20006@naver.com>
2026-04-23 08:39:29 +0900

    [Unrated] Title: 눈치게임 A+B! A-B! A+B! 터렛! A+B! 피보나치 함수! A+B! A-B! A+B! 어린 왕자! A+B! ACM Craft! A+B! A-B! A+B! 습격자 초라기! A+B! 벡터 매칭! A+B! A-B! A+B! A/B! A+B! 터렛! A+B! A-B! A+B! 분산처리! A+B! A+B! 마셔라! 마셔라 마셔라! 마셔라 틀이 들어간다!, Time: 0 ms, Memory: 2020 KB

--- 백준/Unrated/18825. 눈치게임 A+B! A-B! A+B! 터렛! A+B! 피보나치 함수! A+B! A-B! A+B! 어린 왕자! A+B! ACM Craft! A+B! A-B! A+B! 습격자 초라기! A+B! 벡터 매칭! A+B! A-B! A+B! AB! A+B! 터렛! A+B! A-B! A+B! 분산처리! A+B! A+B! 마셔라! 마셔라 마셔라! 마셔라 틀이 들어간다!/눈치게임 A+B! A-B! A+B! 터렛! A+B! 피보나치 함수! A+B! A-B! A+B! 어린 왕자! A+B! ACM Craft! A+B! A-B! A+B! 습격자 초라기! A+B! 벡터 매칭! A+B! A-B! A+B! AB! A+B! 터렛! A+B! A-B! A+B! 분산처리! A+B! A+B! 마셔라! 마셔라 마셔라! 마셔라 틀이 들어간다!.cpp
+++ 백준/Unrated/18825. 눈치게임 A+B! A-B! A+B! 터렛! A+B! 피보나치 함수! A+B! A-B! A+B! 어린 왕자! A+B! ACM Craft! A+B! A-B! A+B! 습격자 초라기! A+B! 벡터 매칭! A+B! A-B! A+B! AB! A+B! 터렛! A+B! A-B! A+B! 분산처리! A+B! A+B! 마셔라! 마셔라 마셔라! 마셔라 틀이 들어간다!/눈치게임 A+B! A-B! A+B! 터렛! A+B! 피보나치 함수! A+B! A-B! A+B! 어린 왕자! A+B! ACM Craft! A+B! A-B! A+B! 습격자 초라기! A+B! 벡터 매칭! A+B! A-B! A+B! AB! A+B! 터렛! A+B! A-B! A+B! 분산처리! A+B! A+B! 마셔라! 마셔라 마셔라! 마셔라 틀이 들어간다!.cpp
@@ -1,43 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-using ll = long long;
-
 int main(){
-    cin.tie(0)->sync_with_stdio(0);
-
-    int N, M;
-    cin>>N>>M;
-    int sx, sy;
-    char gift = 'A';
-    char arr[50][50]{};
-    for(int i=0;i<N;i++)for(int j=0;j<M;j++){
-        cin>>arr[i][j];
-        if(arr[i][j] == 'S')    sx = i, sy = j;
-        else if(arr[i][j] == 'C')   arr[i][j] = ++gift;
-    }
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
-
-    int D[50][50][4][4]{};
-    queue<tuple<int, int, int, int, int>> Q;
-    for(int i=0;i<4;i++){
-        int x = sx+dx[i], y = sy+dy[i];
-        if(x<0 || x>=N || y<0 || y>=M || arr[x][y] == '#')  continue;
-        Q.emplace(x,y,arr[x][y]=='.'?0:arr[x][y]-'A',i,1);
-    }
-    while(!Q.empty()){
-        auto [x,y,g,p,t] = Q.front();   Q.pop();
-        if(g == 3) return cout<<t,0;
-        for(int i=0;i<4;i++){
-            int xx=x+dx[i], yy=y+dy[i];
-            if(xx<0 || xx>=N || yy<0 || yy>=M || arr[xx][yy] == '#' || i == p)  continue;
-            int ng = g;
-            if(arr[xx][yy] != 'S' && arr[xx][yy] != '.')    ng |= (arr[xx][yy] - 'A');
-            if(D[xx][yy][ng][i])    continue;
-            D[xx][yy][ng][i] = t+1;
-            Q.emplace(xx,yy,ng,i,t+1);
-        }
-    }
-    cout<<-1;
-
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
 }
