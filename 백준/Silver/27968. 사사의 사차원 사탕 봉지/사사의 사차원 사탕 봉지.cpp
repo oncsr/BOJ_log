8428d729128d94fa3f86b031668493c2fd23850f
oncsr <khj20006@naver.com>
2026-04-23 07:21:53 +0900

    [Silver 2] Title: 사사의 사차원 사탕 봉지, Time: 148 ms, Memory: 4244 KB

--- 백준/Silver/27968. 사사의 사차원 사탕 봉지/사사의 사차원 사탕 봉지.cpp
+++ 백준/Silver/27968. 사사의 사차원 사탕 봉지/사사의 사차원 사탕 봉지.cpp
@@ -1,28 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 using ll = long long;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
 	int N, M;
 	cin >> N >> M;
-	vector<ll> A(M + 1);
-
-	// 누적 합
-	for (int i = 1; i <= M; i++)
-	{
-		cin >> A[i];
-		A[i] += A[i - 1];
+	ll arr[300000]{};
+	for (int i = 0; i < M; i++) {
+		cin >> arr[i];
+		if (!i)	continue;
+		arr[i] += arr[i - 1];
 	}
-
-	for (; N--;)
-	{
-		ll b;	cin >> b;
-
-		int pos = lower_bound(A.begin(), A.end(), b) - A.begin();
-		if (pos == M + 1)	cout << "Go away!\n";
-		else	cout << pos << '\n';
+	for (int i = 0; i < N; i++) {
+		ll a;
+		cin >> a;
+		int g = lower_bound(arr, arr + M, a) - arr;
+		if (g == M)	cout << "Go away!\n";
+		else	cout << g + 1 << '\n';
 	}
-
 }
