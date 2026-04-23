21eae6003b5e20516a9244304ba4a9b338579ffd
oncsr <khj20006@naver.com>
2026-04-23 09:23:59 +0900

    [Bronze 3] Title: 2017 아주대학교 프로그래밍 경시대회 (Small), Time: 0 ms, Memory: 2024 KB

---
+++ 백준/Bronze/14592. 2017 아주대학교 프로그래밍 경시대회 (Small)/2017 아주대학교 프로그래밍 경시대회 (Small).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+using iii = pair<pair<int, int>, pair<int, int> >;
+bool cmp(iii a, iii b) {
+	if (a.first.first == b.first.first) {
+		if (a.first.second == b.first.second) {
+			return a.second.first < b.second.first;
+		}
+		return a.first.second < b.first.second;
+	}
+	return a.first.first > b.first.first;
+}
+
+int main() {
+	int n;
+	iii arr[3]{};
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		arr[i] = { {a,b},{c,i + 1} };
+	}
+	sort(arr, arr + n, cmp);
+	cout << arr[0].second.second;
+}
