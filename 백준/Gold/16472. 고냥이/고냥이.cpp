8098f07cfba03afe8215dfc0e503247c0741b674
oncsr <khj20006@naver.com>
2026-04-23 09:01:52 +0900

    [Gold 4] Title: 고냥이, Time: 0 ms, Memory: 2392 KB

--- 백준/Gold/16472. 고냥이/고냥이.cpp
+++ 백준/Gold/16472. 고냥이/고냥이.cpp
@@ -1,28 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
-	int N;
-	string A;
-	cin >> N >> A;
-
-	int s = 0, e = 0, ex[26]{}, ans = 1, cnt = 1;
-	ex[A[0] - 'a']++;
-	while (e < A.size()) {
+	string a;
+	int arr[26]{}, N, ans = 0, s = 0, e = 0, cnt = 0;
+	cin >> N >> a;
+	while (e < a.size()) {
+		if (!arr[a[e] - 'a'])	cnt++;
+		arr[a[e] - 'a']++;
 		if (cnt > N) {
-			ex[A[s] - 'a']--;
-			if (ex[A[s] - 'a'] == 0)	cnt--;
-			s++;
+			while (arr[a[s] - 'a']--) {
+				if (arr[a[s] - 'a'] == 0) {
+					cnt--;
+					s++;
+					break;
+				}
+				s++;
+			}
 		}
-		else {
-			e++;
-			if (e == A.size())	break;
-			if (!ex[A[e] - 'a']++)	cnt++;
-		}
-		if (cnt <= N)	ans = max(ans, e - s + 1);
+		ans = max(ans, e - s + 1);
+		e++;
 	}
 	cout << ans;
-
 }
