756a926536ca392e75935cd07262d7769217a8d9
oncsr <khj20006@naver.com>
2026-04-23 11:26:26 +0900

    [Silver 5] Title: 수 정렬하기 2, Time: 316 ms, Memory: 9832 KB

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cpp
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cpp
@@ -1,24 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+typedef long long ll;
+
+int tree[1000001];
+int srt[1000001];
+
+void merge_sort(int start, int mid, int end) {
+	int left, right;
+	int tem = start;
+	left = start;
+	right = mid + 1;
+	while (left <= mid && right <= end) {
+		if (tree[right] < tree[left]) {
+			srt[tem++] = tree[right++];
+		}
+		else {
+			srt[tem++] = tree[left++];
+		}
+	}
+	if (left <= mid) {
+		for (int i = left; i <= mid; i++)
+			srt[tem++] = tree[i];
+	}
+	else {
+		for (int i = right; i <= end; i++)
+			srt[tem++] = tree[i];
+	}
+	for (int i = start; i <= end; i++)
+		tree[i] = srt[i];
+}
+
+void merge(int start, int end) {
+	if (start < end) {
+		int mid = (start + end) / 2;
+		merge(start, mid);
+		merge(mid + 1, end);
+		merge_sort(start, mid, end);
+	}
+}
 
 int main() {
-	ios_base::sync_with_stdio(NULL);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
-	int N;
-	cin >> N;
-	vector<int> Vec;
-	for (int i = 0; i < N; i++) {
-		int a;
-		cin >> a;
-		Vec.push_back(a);
-	}
-
-	sort(Vec.begin(), Vec.end());
-
-	for (int i = 0; i < N; i++) {
-		cout << Vec[i] << '\n';
-	}
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> tree[i];
+	merge(1, n);
+	if (n == 1)	cout << tree[1];
+	else
+		for (int i = 1; i <= n; i++)	cout << srt[i] << '\n';
 }
