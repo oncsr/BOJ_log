e791f90e568549b9cb7ffabe0a4916a941c5020a
oncsr <khj20006@naver.com>
2026-04-23 11:19:47 +0900

    [Silver 4] Title: 붙임성 좋은 총총이, Time: 4 ms, Memory: 2164 KB

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cpp
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cpp
@@ -1,3 +1,30 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
-main(){map<string,int> M;M["ChongChong"]=1;int N,s=1;for(cin>>N;N--;){string a,b;cin>>a>>b;int c=M[a]+M[b];if(c)s+=2-c,M[a]=M[b]=1;}cout<<s;}
+int main() {
+	map<string, bool> M;
+	string a, b;
+	int N, s = 1;
+	for (cin >> N; N--;) {
+		cin >> a >> b;
+		if (!M[a] && !M[b]) {
+			if (a == "ChongChong") {
+				s++;
+				M[b] = 1;
+				M[a] = 1;
+			}
+			if (b == "ChongChong") {
+				s++;
+				M[a] = 1;
+				M[b] = 1;
+			}
+		}
+		else if (M[a] && M[b])	continue;
+		else {
+			M[a] = 1;
+			M[b] = 1;
+			s++;
+		}
+	}
+	cout << s;
+}
