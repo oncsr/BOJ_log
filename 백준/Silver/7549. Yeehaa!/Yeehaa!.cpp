074f2b31d3fbb681dde72d942a66f1009bf46f7c
oncsr <khj20006@naver.com>
2026-04-27 17:20:24 +0900

    [Silver 1] Title: Yeehaa!, Time: 4 ms, Memory: 2108 KB

---
+++ 백준/Silver/7549. Yeehaa!/Yeehaa!.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int N, n;
+	cin >> N;
+	cout.setf(ios::fixed);
+	cout.precision(3);
+	for (int i = 1; i <= N; i++) {
+		cout << "Scenario #" << i << ":\n";
+		double R;
+		cin >> R >> n;
+		cout << R * sin(M_PI / n) / (1 + sin(M_PI / n)) << "\n\n";
+	}
+	cout.unsetf(ios::fixed);
+}
